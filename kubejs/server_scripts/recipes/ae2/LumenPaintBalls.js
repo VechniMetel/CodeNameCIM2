87ccdc1391ad941601ce27851587dec4f55409ad
createdeco <oooooooo_zane@163.com>
2025-03-19 22:59:34 +0800

    CMI Alpha 1.6.8 hotfix
    Fixed a question of the chance of four kinds of lumen paint balls was too high when crushing a black lumen paint ball 修复了粉碎黑色光通染色球时四种光通染色球概率过大的问题

--- kubejs/server_scripts/recipes/ae2/LumenPaintBalls.js
+++ kubejs/server_scripts/recipes/ae2/LumenPaintBalls.js
@@ -27,13 +27,13 @@ ServerEvents.recipes((event) => {
 	})
 	create.crushing([
 		Item.of("ae2:white_lumen_paint_ball"
-).withChance(25.0),
+).withChance(0.25),
 		Item.of("ae2:red_lumen_paint_ball"
-).withChance(25.0),
+).withChance(0.25),
 		Item.of("ae2:blue_lumen_paint_ball"
-).withChance(25.0),
+).withChance(0.25),
 		Item.of("ae2:green_lumen_paint_ball"
-).withChance(25.0),
+).withChance(0.25),
 	], [
 		"ae2:black_lumen_paint_ball"
 	]).processingTime(40)
