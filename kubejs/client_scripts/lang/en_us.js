1ca8dd6e10d9d5b37cc266011ed58f9fbd660d92
VechniMetel <3512546747@qq.com>
2025-03-22 18:26:54 +0800

    Update en_us.js
    finishing en_us translations

--- kubejs/client_scripts/lang/en_us.js
+++ kubejs/client_scripts/lang/en_us.js
@@ -10,7 +10,7 @@ ClientEvents.lang("en_us", (event) => {
 	addMechanismPartLanguaue("magical", "Magical")
 	addMechanismPartLanguaue("quantum", "Quantum")
 	addMechanismPartLanguaue("engineering", "Engineering")
-	addMechanismPartLanguaue("mekanism", "Generic")
+	addMechanismPartLanguaue("mekanism", "Meka")
 	addMechanismPartLanguaue("final", "Final")
 
 	addFluidLanguaue("molten_andesite_alloy", "Molten andesite alloy")
@@ -40,74 +40,68 @@ ClientEvents.lang("en_us", (event) => {
     addCommonTooltipLanguage("ue_addons:geographycal_hammer.developer2", "Right click with this in off hand to get id of your main hand item")
     addCommonTooltipLanguage("ue_addons:geographycal_hammer.not_developer", "It seems that a mysterious power has stopped you from using this tool")
 
-    addAccelerateTooltipLanguage("ue_addons:stone_mechanism", "§eStone transforming\n10%Andesite\n10%Diorite\n10%Granite\n10%Limestone\n10%焦黑石头\n§2Deepslate transforming\n10%焦黑石头\n10%凝灰岩\n10%熔渣\n10%滴水石块\n10%平滑玄武岩")
-    addAccelerateTooltipLanguage("ue_addons:andesite_mechanism", "§eStone transforming\n20%Andesite\n5%Iron ore\n5%Stone\n§2Deepslate transforming\n15%安山岩\n10%深层铁矿石\n10%深板岩")
-    addAccelerateTooltipLanguage("ue_addons:copper_mechanism", "§eStone transforming\n20%铜矿石\n5%金矿石\n5%粘土块\n§2Deepslate transforming\n15%深层铜矿石\n10%深层金矿石\n10%粘土块")
-    addAccelerateTooltipLanguage("ue_addons:iron_mechanism", "§eStone transforming\n20%铁矿石\n5%红石矿石\n5%镍矿石\n§2Deepslate transforming\n15%深层铁矿石\n10%深层红石矿\n10%深层镍矿石")
-    addAccelerateTooltipLanguage("ue_addons:gold_mechanism", "§eStone transforming\n20%金矿石\n5%朱砂矿石\n5%石英矿石\n§2Deepslate transforming\n15%深层金矿石\n10%深层朱砂矿\n10%深层石英矿")
+    addAccelerateTooltipLanguage("ue_addons:stone_mechanism", "§eStone transforming\n10%Andesite\n10%Diorite\n10%Granite\n10%Limestone\n10%Seared stone\n§2Deepslate transforming\n10%Seared stone\n10%Tuff\n10%Scoria\n10%Dripstone block\n10%Smooth basalt")
+    addAccelerateTooltipLanguage("ue_addons:andesite_mechanism", "§eStone transforming\n20%Andesite\n5%Iron ore\n5%Stone\n§2Deepslate transforming\n15%Andesite\n10%Deepslate iron ore\n10%Deepslate")
+    addAccelerateTooltipLanguage("ue_addons:copper_mechanism", "§eStone transforming\n20%Copper ore\n5%Gold ore\n5%Clay\n§2Deepslate transforming\n15%Deepslate copper oren10%Deepslate gold ore\n10%Clay")
+    addAccelerateTooltipLanguage("ue_addons:iron_mechanism", "§eStone transforming\n20%Iron ore\n5%Redstone ore\n5%Nickel ore\n§2Deepslate transforming\n15%Deepslate iron ore\n10%Deepslate redstone ore\n10%Deepslate nickel ore")
+    addAccelerateTooltipLanguage("ue_addons:gold_mechanism", "§eStone transforming\n20%Gold ore\n5%Cinnabar ore\n5%Quartz ore\n§2Deepslate transforming\n15%Deepslate gold ore\n10%Deepslate cinnabar ore\n10%Deepslate quartz ore")
     addAccelerateTooltipLanguage("ue_addons:ender_mechanism", "§5Teleporting player to stored location")
 
     addHeatedLanguaue("grilled", "grilling")
 
-	addMusicDiscLanguaue("lan_huo", "烂活", "霜落落落霜")
+	addMusicDiscLanguaue("lan_huo", "Strange troll", "霜落落落霜")
 	addMusicDiscLanguaue("monody", "Monody", "TheFatRat")
 	addMusicDiscLanguaue("life", "Life", "Neuro Sama")
-	addMusicDiscLanguaue("soul", "鬼", "草东没有派对")
-	addMusicDiscLanguaue("cylinder", "缸", "草东没有派对")
-	addMusicDiscLanguaue("but", "但", "草东没有派对")
-	addMusicDiscLanguaue("dirt", "烂泥", "草东没有派对")
-
-	addBlockLanguaue("qio_empty", "空的QIO驱动器盒")
-	addBlockLanguaue("qio_one", "QIO驱动器盒")
-	addBlockLanguaue("qio_two", "超密集QIO驱动器盒")
-	addBlockLanguaue("qio_three", "时间膨胀QIO驱动器盒")
-	addBlockLanguaue("qio_four", "超质量QIO驱动器盒")
+	addMusicDiscLanguaue("soul", "Soul", "草东没有派对")
+	addMusicDiscLanguaue("cylinder", "Cylinder", "草东没有派对")
+	addMusicDiscLanguaue("but", "But", "草东没有派对")
+	addMusicDiscLanguaue("dirt", "Dirt", "草东没有派对")
 
 	let materialGroup = [
-		["nature", "自然"],
-		["wooden", "木质"],
-		["stone", "石质"],
-		["iron", "铁质"],
-		["copper", "铜质"],
-		["andesite", "安山"],
-		["photosensitive", "感光"],
-		["gold", "金质"],
-		["cobalt", "钴质"],
-		["thermal", "热力"],
-		["feinforced", "强化"],
-		["resonant", "谐振"],
-		["railway", "铁路"],
-		["ender", "末影"],
-		["light_engineering", "轻型工程"],
-		["heavy_engineering", "重型工程"],
-		["enchanted", "附魔"],
-		["smart", "智能"],
-		["basic_mekanism", "基础通用"],
-		["advanced_mekanism", "高级通用"],
-		["elite_mekanism", "精英通用"],
-		["ultimate_mekanism", "终极通用"],
-		["structure", "结构"],
-		["nuclear", "核"],
-		["antimatter", "反物质"],
-		["coil", "线圈"],
-		["sculk", "幽匿"],
-		["colorful", "多彩"],
-		["pigiron", "生铁"],
-		["creative", "创造"]
+		["nature", "Natural"],
+		["wooden", "Wooden"],
+		["stone", "Stone"],
+		["iron", "iron"],
+		["copper", "Copper"],
+		["andesite", "Andesite"],
+		["photosensitive", "Photosensitive"],
+		["gold", "Golden"],
+		["cobalt", "Cobalt"],
+		["thermal", "Thermal"],
+		["feinforced", "Feinforced"],
+		["resonant", "Resonant"],
+		["railway", "Railway"],
+		["ender", "Ender"],
+		["light_engineering", "Light engineering"],
+		["heavy_engineering", "Heavy engineering"],
+		["enchanted", "Enchanted"],
+		["smart", "Smart"],
+		["basic_mekanism", "Basic meka"],
+		["advanced_mekanism", "Advanced meka"],
+		["elite_mekanism", "Elite meka"],
+		["ultimate_mekanism", "Ultimate meka"],
+		["structure", "Structure"],
+		["nuclear", "Nuclear"],
+		["antimatter", "Antimatter"],
+		["coil", "Coil"],
+		["sculk", "Sculk"],
+		["colorful", "Colorful"],
+		["pigiron", "Pigiron"],
+		["creative", "Creative"]
 	]
 	materialGroup.forEach(([material, name]) => {
 		addMechanismLanguaue(material, name)
 	})
 
 	let completeMaterialGroup = [
-		["basic_random", "基础随机"],
-		["mechanical_random", "机械随机"],
-		["engineering_random", "工程随机"],
-		["flux_random", "通量随机"],
-		["magical_random", "魔力随机"],
-		["quantum_random", "量子随机"],
-		["mekanism_random", "通用随机"],
-		["final_random", "最终随机"]
+		["basic_random", "Basic random"],
+		["mechanical_random", "Mechanical random"],
+		["engineering_random", "Engineering random"],
+		["flux_random", "Flux random"],
+		["magical_random", "Magical random"],
+		["quantum_random", "Quantum random"],
+		["mekanism_random", "Meka random"],
+		["final_random", "Final random"]
 	]
 	completeMaterialGroup.forEach(([material, name]) => {
 		addCompleteMechanismLanguaue(material, name)
@@ -117,22 +111,22 @@ ClientEvents.lang("en_us", (event) => {
 	}
 
 	function addCastItemLanguaue(key, value) {
-		event.add(`item.${global.namespace}.${key}_cast`, `${value}铸模`)
-		event.add(`item.${global.namespace}.${key}_sand_cast`, `${value}沙子铸模`)
-		event.add(`item.${global.namespace}.${key}_red_sand_cast`, `${value}红沙铸模`)
+		event.add(`item.${global.namespace}.${key}_cast`, `${value} cast`)
+		event.add(`item.${global.namespace}.${key}_sand_cast`, `${value} sand cast`)
+		event.add(`item.${global.namespace}.${key}_red_sand_cast`, `${value} red sand cast`)
 	}
 
 	function addMechanismLanguaue(material, value) {
-		event.add(`item.${global.namespace}.${material}_mechanism`, `${value}构件`)
-		event.add(`item.${global.namespace}.incomplete_${material}_mechanism`, `${value}构件(半成品)`)
+		event.add(`item.${global.namespace}.${material}_mechanism`, `${value} mechanism`)
+		event.add(`item.${global.namespace}.incomplete_${material}_mechanism`, `${value} mechanism(incomplete)`)
 	}
 
 	function addCompleteMechanismLanguaue(material, value) {
-		event.add(`item.${global.namespace}.${material}_mechanism`, `${value}构件`)
+		event.add(`item.${global.namespace}.${material}_mechanism`, `${value} mechanism`)
 	}
 
 	function addMechanismPartLanguaue(material, value) {
-		event.add(`item.${global.namespace}.${material}_mechanism_part`, `${value}构件零件`)
+		event.add(`item.${global.namespace}.${material}_mechanism_part`, `${value} mechanism part`)
 	}
 
 	function addBlockLanguaue(key, value) {
@@ -142,7 +136,7 @@ ClientEvents.lang("en_us", (event) => {
 	function addFluidLanguaue(key, value) {
 		event.add(`fluid.${global.namespace}.${key}`, value)
 		event.add(`block.${global.namespace}.${key}`, value)
-		event.add(`item.${global.namespace}.${key}_bucket`, `${value}桶`)
+		event.add(`item.${global.namespace}.${key}_bucket`, `${value} bucket`)
 	}
 
 	function addCommonTooltipLanguage(key, value) {
