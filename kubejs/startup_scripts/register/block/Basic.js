1cf4f205c368fecbfbc2189b914ea23751af59c3
VechniMetel <3512546747@qq.com>
2025-03-22 21:08:06 +0800

    deleting qio

--- kubejs/startup_scripts/register/block/Basic.js
+++ kubejs/startup_scripts/register/block/Basic.js
@@ -1,20 +1,4 @@
 StartupEvents.registry("block", (event) => {
 	event.create(`${global.namespace}:the_accelerator_of_mechanism_power`)
 		.soundType(SoundType.METAL)
-
-	let registerQio = [
-		"one",
-		"two",
-		"three",
-		"four",
-		"empty"
-	]
-	registerQio.forEach((name) => {
-		event.create(`${global.namespace}:qio_${name}`)
-			.soundType(SoundType.METAL)
-			.model(`${global.namespace}:block/qio/${name}`)
-			.tagBlock(global.miningLevel["stone"])
-			.tagBlock(global.toolType["pickaxe"])
-			.requiresTool(true)
-	})
 })
