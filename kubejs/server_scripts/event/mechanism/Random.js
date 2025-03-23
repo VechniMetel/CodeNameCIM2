94d778c54f442d4da01c9eb81b49c08304019443
createdeco <oooooooo_zane@163.com>
2025-03-23 20:25:53 +0800

    Revert "CIM Alpha 1.7.4 ???"
    This reverts commit 7539031007826a04014355a04022022a18feb6e1.

--- kubejs/server_scripts/event/mechanism/Random.js
+++ kubejs/server_scripts/event/mechanism/Random.js
@@ -15,17 +15,19 @@ ItemEvents.rightClicked((event) => {
 			if (player === null) {
 				return
 			}
-			let lootTable = `ue_addons:gameplay/random_mechanisms/${material}`
-			let { x, y, z } = player.pos
-			server.runCommandSilent("loot give " + player.username + " loot " + lootTable)
-			player.swing()
-			player.playNotifySound("create:crafter_craft", "voice", 2, 1)
-			let R = Math.random()
-			let G = Math.random()
-			let B = Math.random()
-			server.runCommandSilent(`particle minecraft:dust ${R} ${G} ${B} 1 ${x} ${y + 1.5} ${z} 0.5 0.5 0.5 0.1 30`)
-			if (!player.isCreative()) {
-				item.shrink(1)
+			if (event.hand === "MAIN_HAND") {
+				let lootTable = `ue_addons:gameplay/random_mechanisms/${material}`
+				let { x, y, z } = player.pos
+				server.runCommandSilent("loot give " + player.username + " loot " + lootTable)
+				player.swing()
+				player.playNotifySound("create:crafter_craft", "voice", 2, 1)
+				let R = Math.random()
+				let G = Math.random()
+				let B = Math.random()
+				server.runCommandSilent(`particle minecraft:dust ${R} ${G} ${B} 1 ${x} ${y + 1.5} ${z} 0.5 0.5 0.5 0.1 30`)
+				if (!player.isCreative()) {
+					item.shrink(1)
+				}
 			}
 		}
 	})
