4f6de23ff0b8fda667304ec4cc39c8deef37fe9a
VechniMetel <3512546747@qq.com>
2025-03-25 21:22:30 +0800

    awa

--- kubejs/startup_scripts/register/Materials.js
+++ kubejs/startup_scripts/register/Materials.js
@@ -1,4 +1,3 @@
-@ -1,184 +1,207 @@
 let addColorMaterial = [
 	// 安山合金
 	["andesite_alloy", 0xA9AFA1, "wooden"],
@@ -54,161 +53,130 @@ let addColorMaterial = [
 	["vanadium", 0xF0FFFF, "iron"]
 ]
 
-let addAloneMaterial = []
-
-let addColorMoltenFluid = [
-let addPlate = [
-	// 流明
-	["lumium", 0xFCEEA8],
-	// 信素 
-	["signalum", 0xF86014],
-	// 末影
-	["enderium", 0x1D7D8A]
-]
-let addAlonePlate = []
+	let addPlate = [
+		// 流明
+		["lumium", 0xFCEEA8],
+		// 信素 
+		["signalum", 0xF86014],
+		// 末影
+		["enderium", 0x1D7D8A]
+	]
 
 let addMoltenFluid = [
-	["andesite_alloy", 0xA9AFA1],
-	["vanadium", 0xF0FFFF]
-]
+		["andesite_alloy", 0xA9AFA1],
+		["vanadium", 0xF0FFFF]
+	]
 
-let addAloneMoltenFluid = []
 StartupEvents.registry("item", (event) => {
-	addColorMaterial.forEach(([name, color]) => {
-		event.create(`${global.namespace}:${name}_ingot`)
-			.color(color)
-			.texture(`${global.namespace}:item/material/color/ingot`)
-			.tag("forge:ingots")
-			.tag(`forge:ingots/${name}`)
-			.tag("forge:ingots/metal")
-			.tag(`forge:ingots/metal/${name}`)
+		addColorMaterial.forEach(([name, color]) => {
+			event.create(`${global.namespace}:${name}_ingot`)
+				.color(color)
+				.texture(`${global.namespace}:item/material/color/ingot`)
+				.tag("forge:ingots")
+				.tag(`forge:ingots/${name}`)
+				.tag("forge:ingots/metal")
+				.tag(`forge:ingots/metal/${name}`)
 
-		event.create(`${global.namespace}:${name}_nugget`)
-			.color(color)
-			.texture(`${global.namespace}:item/material/color/nugget`)
-			.tag("forge:nuggets")
-			.tag(`forge:nuggets/${name}`)
-			.tag("forge:nuggets/metal")
-			.tag(`forge:nuggets/metal/${name}`)
+			event.create(`${global.namespace}:${name}_nugget`)
+				.color(color)
+				.texture(`${global.namespace}:item/material/color/nugget`)
+				.tag("forge:nuggets")
+				.tag(`forge:nuggets/${name}`)
+				.tag("forge:nuggets/metal")
+				.tag(`forge:nuggets/metal/${name}`)
 
-		event.create(`${global.namespace}:${name}_plate`)
-			.color(color)
-			.texture(`${global.namespace}:item/material/color/plate`)
-			.tag("forge:plates")
-			.tag(`forge:plates/${name}`)
-			.tag("forge:plates/metal")
-			.tag(`forge:plates/metal/${name}`)
+			event.create(`${global.namespace}:${name}_plate`)
+				.color(color)
+				.texture(`${global.namespace}:item/material/color/plate`)
+				.tag("forge:plates")
+				.tag(`forge:plates/${name}`)
+				.tag("forge:plates/metal")
+				.tag(`forge:plates/metal/${name}`)
+		})
+
+		addAloneMaterial.forEach(([name]) => {
+			event.create(`${global.namespace}:${name}_ingot`)
+				.texture(`${global.namespace}:item/material/alone/${name}/ingot`)
+				.tag("forge:ingots")
+				.tag(`forge:ingots/${name}`)
+				.tag("forge:ingots/metal")
+				.tag(`forge:ingots/metal/${name}`)
+
+			event.create(`${global.namespace}:${name}_nugget`)
+				.texture(`${global.namespace}:item/material/alone/${name}/nugget`)
+				.tag("forge:nuggets")
+				.tag(`forge:nuggets/${name}`)
+				.tag("forge:nuggets/metal")
+				.tag(`forge:nuggets/metal/${name}`)
+
+			event.create(`${global.namespace}:${name}_plate`)
+				.texture(`${global.namespace}:item/material/alone/${name}/plate`)
+				.tag("forge:plates")
+				.tag(`forge:plates/${name}`)
+				.tag("forge:plates/metal")
+				.tag(`forge:plates/metal/${name}`)
+		})
+		addPlate.forEach(([name, color]) => {
+			event.create(`${global.namespace}:${name}_plate`)
+				.color(color)
+				.texture(`${global.namespace}:item/material/color/plate`)
+				.tag("forge:plates")
+				.tag(`forge:plates/${name}`)
+				.tag("forge:plates/metal")
+				.tag(`forge:plates/metal/${name}`)
+		})
+		addAlonePlate.forEach(([name]) => {
+			event.create(`${global.namespace}:${name}_plate`)
+				.texture(`${global.namespace}:item/material/alone/${name}/plate`)
+				.tag("forge:plates")
+				.tag(`forge:plates/${name}`)
+				.tag("forge:plates/metal")
+				.tag(`forge:plates/metal/${name}`)
+		})
 	})
-
-	addAloneMaterial.forEach(([name]) => {
-		event.create(`${global.namespace}:${name}_ingot`)
-			.texture(`${global.namespace}:item/material/alone/${name}/ingot`)
-			.tag("forge:ingots")
-			.tag(`forge:ingots/${name}`)
-			.tag("forge:ingots/metal")
-			.tag(`forge:ingots/metal/${name}`)
-
-		event.create(`${global.namespace}:${name}_nugget`)
-			.texture(`${global.namespace}:item/material/alone/${name}/nugget`)
-			.tag("forge:nuggets")
-			.tag(`forge:nuggets/${name}`)
-			.tag("forge:nuggets/metal")
-			.tag(`forge:nuggets/metal/${name}`)
-
-		event.create(`${global.namespace}:${name}_plate`)
-			.texture(`${global.namespace}:item/material/alone/${name}/plate`)
-			.tag("forge:plates")
-			.tag(`forge:plates/${name}`)
-			.tag("forge:plates/metal")
-			.tag(`forge:plates/metal/${name}`)
-	})
-	addPlate.forEach(([name,color])=>{
-		event.create(`${global.namespace}:${name}_plate`)
-			.color(color)
-			.texture(`${global.namespace}:item/material/color/plate`)
-			.tag("forge:plates")
-			.tag(`forge:plates/${name}`)
-			.tag("forge:plates/metal")
-			.tag(`forge:plates/metal/${name}`)
-	})
-	addAlonePlate.forEach(([name])=>{
-		event.create(`${global.namespace}:${name}_plate`)
-			.texture(`${global.namespace}:item/material/alone/${name}/plate`)
-			.tag("forge:plates")
-			.tag(`forge:plates/${name}`)
-			.tag("forge:plates/metal")
-			.tag(`forge:plates/metal/${name}`)
-	})
-})
 
 StartupEvents.registry("block", (event) => {
-	addColorMaterial.forEach(([name, color, level]) => {
-		event.create(`${global.namespace}:${name}_block`)
-			.color(color)
-			.hardness(5)
-			.resistance(5)
-			.soundType(SoundType.METAL)
-			.textureAll(`${global.namespace}:block/material/color/storage_blocks`)
-			.tag("forge:storage_blocks")
-			.tag(`forge:storage_blocks/${name}`)
-			.tag("forge:storage_blocks/metal")
-			.tag(`forge:storage_blocks/metal/${name}`)
-			.tagBlock(global.toolType["pickaxe"])
-			.tagBlock(global.miningLevel[level])
-			.requiresTool(true)
-			.item((event) => {
-				event.color(color)
-			})
-	})
+		addColorMaterial.forEach(([name, color, level]) => {
+			event.create(`${global.namespace}:${name}_block`)
+				.color(color)
+				.hardness(5)
+				.resistance(5)
+				.soundType(SoundType.METAL)
+				.textureAll(`${global.namespace}:block/material/color/storage_blocks`)
+				.tag("forge:storage_blocks")
+				.tag(`forge:storage_blocks/${name}`)
+				.tag("forge:storage_blocks/metal")
+				.tag(`forge:storage_blocks/metal/${name}`)
+				.tagBlock(global.toolType["pickaxe"])
+				.tagBlock(global.miningLevel[level])
+				.requiresTool(true)
+				.item((event) => {
+					event.color(color)
+				})
+		})
 
-	addAloneMaterial.forEach(([name, level]) => {
-		event.create(`${global.namespace}:${name}_block`)
-			.hardness(5)
-			.resistance(5)
-			.soundType(SoundType.METAL)
-			.textureAll(`${global.namespace}:block/material/alone/${name}/storage_blocks`)
-			.tag("forge:storage_blocks")
-			.tag(`forge:storage_blocks/${name}`)
-			.tag("forge:storage_blocks/metal")
-			.tag(`forge:storage_blocks/metal/${name}`)
-			.tagBlock(global.toolType["pickaxe"])
-			.tagBlock(global.miningLevel[level])
-			.requiresTool(true)
+		addAloneMaterial.forEach(([name, level]) => {
+			event.create(`${global.namespace}:${name}_block`)
+				.hardness(5)
+				.resistance(5)
+				.soundType(SoundType.METAL)
+				.textureAll(`${global.namespace}:block/material/alone/${name}/storage_blocks`)
+				.tag("forge:storage_blocks")
+				.tag(`forge:storage_blocks/${name}`)
+				.tag("forge:storage_blocks/metal")
+				.tag(`forge:storage_blocks/metal/${name}`)
+				.tagBlock(global.toolType["pickaxe"])
+				.tagBlock(global.miningLevel[level])
+				.requiresTool(true)
+		})
 	})
-})
 
 StartupEvents.registry("fluid", (event) => {
-	addColorMoltenFluid.forEach(([name, color]) => {
-	addMoltenFluid.forEach(([name, color]) => {
-		event.create(`${global.namespace}:molten_${name}`)
-			.thinTexture(color)
-			.bucketColor(color)
-			.flowingTexture(`${global.namespace}:block/fluid/flowing`)
-			.stillTexture(`${global.namespace}:block/fluid/still`)
-			.tag("forge:molten_materials")
-			.tag(`forge:molten_${name}`)
-			.tag(`tconstruct:molten_${name}`)
-
-		// 生成Json模型文件
-		JsonIO.write(`kubejs/assets/${global.namespace}/models/item/molten_${name}_bucket.json`, {
-			"parent": "forge:item/bucket_drip",
-			"loader": "forge:fluid_container",
-			"fluid": `${global.namespace}:molten_${name}`
+		addColorMoltenFluid.forEach(([name, color]) => {
+			addMoltenFluid.forEach(([name, color]) => {
+				event.create(`${global.namespace}:molten_${name}`)
+					.thinTexture(color)
+					.bucketColor(color)
+			})
 		})
 	})
-
-	addAloneMoltenFluid.forEach(([name]) => {
-		event.create(`${global.namespace}:molten_${name}`)
-			.flowingTexture(`${global.namespace}:block/fluid/alone/${name}/flowing`)
-			.stillTexture(`${global.namespace}:block/fluid/alone/${name}/still`)
-			.tag("forge:molten_materials")
-			.tag(`forge:molten_${name}`)
-			.tag(`tconstruct:molten_${name}`)
-
-		JsonIO.write(`kubejs/assets/${global.namespace}/models/item/molten_${name}_bucket.json`, {
-			"parent": "forge:item/bucket_drip",
-			"loader": "forge:fluid_container",
-			"fluid": `${global.namespace}:molten_${name}`
-		})
-	})
-})
