81b40f00c2b60d2a53042535d11a521bf5c596c7
Qi-Month <dy757410@163.com>
2025-03-28 21:22:46 +0800

    修改写法

--- kubejs/server_scripts/recipes/tconstruct/Compat.js
+++ kubejs/server_scripts/recipes/tconstruct/Compat.js
@@ -4,49 +4,21 @@ ServerEvents.recipes((event) => {
 	// 机壳
 	tconstruct.casting_basin(
 		"create:andesite_casing",
-		Fluid.of("ue_addons:molten_andesite_alloy", 90),
-		40,
-		"#minecraft:logs",
-		true
-	)
+		Fluid.of("ue_addons:molten_andesite_alloy", 90)
+	).cast("#minecraft:logs").cast_consumed(true).cooling_time(40)
 
 	tconstruct.casting_basin(
 		"create:copper_casing",
-		Fluid.of("tconstruct:molten_copper", 90),
-		40,
-		"#minecraft:logs",
-		true
-	)
+		Fluid.of("tconstruct:molten_copper", 90)
+	).cast("#minecraft:logs").cast_consumed(true).cooling_time(40)
 
 	tconstruct.casting_basin(
 		"create:brass_casing",
-		Fluid.of("tconstruct:molten_brass", 90),
-		40,
-		"#minecraft:logs",
-		true
-	)
+		Fluid.of("tconstruct:molten_brass", 90)
+	).cast("#minecraft:logs").cast_consumed(true).cooling_time(40)
 
 	tconstruct.casting_basin(
 		"create:railway_casing",
-		Fluid.of("tconstruct:molten_obsidian", 90),
-		40,
-		"create:brass_casing",
-		true
-	)
-
-	tconstruct.casting_basin(
-		"create:andesite_alloy",
-		Fluid.of("tconstruct:molten_iron", 30),
-		20,
-		"minecraft:andesite",
-		true
-	)
-
-	tconstruct.casting_basin(
-		"create:andesite_alloy",
-		Fluid.of("tconstruct:molten_zinc", 10),
-		0,
-		"minecraft:andesite",
-		true
-	)
+		Fluid.of("tconstruct:molten_obsidian", 90)
+	).cast("create:brass_casing").cast_consumed(true).cooling_time(40)
 })
