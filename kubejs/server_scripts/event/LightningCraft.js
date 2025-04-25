ae8578195b447536e752f07bd548a7848df60a7b
Qi-Month <dy757410@163.com>
2025-04-25 20:10:15 +0800

    fix(server_scripts): improve LightningCraft function and update item handling
    - Update addLightningCraft function to properly handle item counts
    - Change input and output items for lightning craft
    - Improve item spawning logic to distribute items randomly around the lightning strike
    - Remove outdated comments and update function documentation

--- kubejs/server_scripts/event/LightningCraft.js
+++ kubejs/server_scripts/event/LightningCraft.js
@@ -2,13 +2,12 @@ EntityEvents.spawned("minecraft:lightning_bolt", (event) => {
 	let lightning = event.entity
 	let { level } = event
 
-	addLightningCraft("minecraft:diamond", "minecraft:iron_ingot")
+	addLightningCraft("ue_addons:bucket", "minecraft:bucket")
 
 	/**
-	 * 函数封装, 由于这只是一个很简单的小合成用于彩蛋, 因此并没有写到最后, 还有一点bug, 例如输出数量和输入数量不对等
-	 * 丢了多个在地上但是只返回1个
-	 * @param {Item} output 输出
-	 * @param {Item} input 输入
+	 * 函数封装
+	 * @param {Internal.Item_} output 输出
+	 * @param {Internal.Item_} input 输入
 	 */
 	function addLightningCraft(output, input) {
 		let targets = level.getEntitiesWithin(AABB.of(
@@ -23,15 +22,20 @@ EntityEvents.spawned("minecraft:lightning_bolt", (event) => {
 			let posY = entity.y
 			let posZ = entity.z
 
+			let count = entity.item.count || 1
+
 			entity.kill()
 
-			let itemEntity = level.createEntity("minecraft:item")
-			itemEntity.item = output
-			itemEntity.x = posX
-			itemEntity.y = posY + 1
-			itemEntity.z = posZ
-			itemEntity.mergeNbt({ Invulnerable: 1 })
-			itemEntity.spawn()
+			for (let i = 0; i < count; i++) {
+				let itemEntity = level.createEntity("minecraft:item")
+				itemEntity.item = Item.of(output)
+
+				itemEntity.x = posX + (Math.random() * 0.4 - 0.2)
+				itemEntity.y = posY + (Math.random() * 0.4 - 0.2)
+				itemEntity.z = posZ + (Math.random() * 0.4 - 0.2)
+				itemEntity.mergeNbt({ Invulnerable: 1 })
+				itemEntity.spawn()
+			}
 		})
 	}
 })
