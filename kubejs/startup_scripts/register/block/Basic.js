f7f0ce8b41fd77c7c9a9698abe29a7f94dad9297
Unknown_Entity_ <Unknown_Entity_@outlook.com>
2025-04-26 15:40:26 +0800

    ponder block

--- kubejs/startup_scripts/register/block/Basic.js
+++ kubejs/startup_scripts/register/block/Basic.js
@@ -1,4 +1,100 @@
 StartupEvents.registry("block", (event) => {
 	event.create(`${global.namespace}:the_accelerator_of_mechanism_power`)
 		.soundType(SoundType.METAL)
-})
+
+		event.create(`${global.namespace}:ponder_thermal_input`)
+			.soundType(SoundType.METAL)
+
+			event.create(`${global.namespace}:ponder_thermal_output`)
+				.soundType(SoundType.METAL)
+
+				event.create(`${global.namespace}:ponder_thermal_output_on`)
+					.soundType(SoundType.METAL)
+
+					event.create(`${global.namespace}:ponder_thermal_input_on`)
+						.soundType(SoundType.METAL)
+
+				event.create(`${global.namespace}:ponder_thermal_fluid`)
+					.soundType(SoundType.METAL)
+
+				event.create(`${global.namespace}:ponder_thermal_fluid_blue`)
+					.soundType(SoundType.METAL)
+
+					event.create(`${global.namespace}:ponder_thermal_fluid_orange`)
+						.soundType(SoundType.METAL)
+						
+						event.create(`${global.namespace}:ponder_thermal_slot`)
+							.soundType(SoundType.METAL)
+							
+							event.create(`${global.namespace}:ponder_thermal_slot_blue`)
+								.soundType(SoundType.METAL)
+
+								event.create(`${global.namespace}:ponder_thermal_slot_orange`)
+									.soundType(SoundType.METAL)
+
+									event.create(`${global.namespace}:ponder_thermal_energy`)
+										.soundType(SoundType.METAL)
+
+										event.create(`${global.namespace}:ponder_thermal_gui`)
+											.soundType(SoundType.METAL)
+
+											event.create(`${global.namespace}:ponder_thermal_gui_blue`)
+												.soundType(SoundType.METAL)
+
+												event.create(`${global.namespace}:ponder_thermal_gui_green`)
+													.soundType(SoundType.METAL)
+
+													event.create(`${global.namespace}:ponder_thermal_gui_red`)
+														.soundType(SoundType.METAL)
+
+														event.create(`${global.namespace}:ponder_thermal_machine_front`)
+															.soundType(SoundType.METAL)
+
+															event.create(`${global.namespace}:ponder_thermal_machine_side`)
+																.soundType(SoundType.METAL)
+
+																event.create(`${global.namespace}:ponder_thermal_machine_side_blue`)
+																	.soundType(SoundType.METAL)
+
+																	event.create(`${global.namespace}:ponder_thermal_machine_side_orange`)
+																		.soundType(SoundType.METAL)
+
+																		event.create(`${global.namespace}:ponder_thermal_machine_side_mix`)
+																			.soundType(SoundType.METAL)
+
+																			event.create(`${global.namespace}:ponder_thermal_machine_side_disabled`)
+																				.soundType(SoundType.METAL)
+
+																				event.create(`${global.namespace}:ponder_thermal_machine_top`)
+																					.soundType(SoundType.METAL)
+
+																					event.create(`${global.namespace}:ponder_thermal_machine_bottom`)
+																						.soundType(SoundType.METAL)
+
+																						event.create(`${global.namespace}:ponder_thermal_rs1_off`)
+																							.soundType(SoundType.METAL)
+
+																							event.create(`${global.namespace}:ponder_thermal_rs2_off`)
+																								.soundType(SoundType.METAL)
+
+																								event.create(`${global.namespace}:ponder_thermal_rs3_off`)
+																									.soundType(SoundType.METAL)
+
+																									event.create(`${global.namespace}:ponder_thermal_rs1_on`)
+																										.soundType(SoundType.METAL)
+
+																										event.create(`${global.namespace}:ponder_thermal_rs2_on`)
+																											.soundType(SoundType.METAL)
+
+																											event.create(`${global.namespace}:ponder_thermal_rs3_on`)
+																												.soundType(SoundType.METAL)
+
+																												event.create(`${global.namespace}:ponder_thermal_lava`)
+																													.soundType(SoundType.METAL)
+
+																													event.create(`${global.namespace}:ponder_thermal_water`)
+																														.soundType(SoundType.METAL)
+
+																														event.create(`${global.namespace}:ponder_thermal_arrow`)
+																															.soundType(SoundType.METAL)
+	})
