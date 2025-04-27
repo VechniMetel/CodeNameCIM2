c0a5e15d7b57f46472b11b8d5dfeeacb52abc22f
createdeco <oooooooo_zane@163.com>
2025-04-27 17:17:21 +0800

    Created 5 machine blocks
    These blocks are from CIM and I modified their models.

--- kubejs/startup_scripts/register/block/Basic.js
+++ kubejs/startup_scripts/register/block/Basic.js
@@ -1,4 +1,22 @@
 StartupEvents.registry("block", (event) => {
 	event.create(`${global.namespace}:the_accelerator_of_mechanism_power`)
 		.soundType(SoundType.METAL)
+		.tagBlock("minecraft:mineable/pickaxe")
+		.tagBlock(global.miningLevel[wooden])
+		.requiresTool(true)
+	
+	let machineBlockRegister = [
+		"andesite",
+		"brass",
+		"copper",
+		"me",
+		"mekanism"
+	]
+	machineBlockRegister.forEach((type)=>{
+		event.create(`${global.namespace}:${type}_machine`)
+		.soundType(SoundType.METAL)
+		.tagBlock("minecraft:mineable/pickaxe")
+		.tagBlock(global.miningLevel[wooden])
+		.requiresTool(true)
+	})
 })
