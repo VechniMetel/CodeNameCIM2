f712e022d9971ae7b048d9079e3289dbaea93570
createdeco <oooooooo_zane@163.com>
2025-04-30 18:09:09 +0800

    忘了加“#”了！

--- kubejs/startup_scripts/register/CreativeTab.js
+++ kubejs/startup_scripts/register/CreativeTab.js
@@ -1,6 +1,6 @@
 StartupEvents.registry("creative_mode_tab", (event) => {
 	event.create(`${global.namespace}:io_block`)
 		.icon(() => "ue_addons:ponder_thermal_input")
-		.content(() => Ingredient.of("ue_addons:io_debug_block").itemIds)
+		.content(() => Ingredient.of("#ue_addons:io_debug_block").itemIds)
 		.displayName(Component.translate(`itemGroup.${global.namespace}.io_block`))
 })
