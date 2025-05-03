0b2005785bc6d2ca944c3a21652017402d061bd2
Unknown_Entity_ <Unknown_Entity_@outlook.com>
2025-05-03 16:21:06 +0800

    model fix

---
+++ kubejs/startup_scripts/register/painting/test.js
@@ -0,0 +1,6 @@
+// StartupEvents.registry("painting_variant", (event) => {
+//	event.create(`${global.namespace}:test`)
+//		.texture(`${global.namespace}:item/material/salt/fecl2`)
+//        .width(1)
+//        .hight(1)
+//})
