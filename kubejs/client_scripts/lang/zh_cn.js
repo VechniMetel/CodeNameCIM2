b7469e2a5e7929d8e93f863c0f989c89b8a62c50
createdeco <oooooooo_zane@163.com>
2025-05-06 19:18:27 +0800

    修复了翻译不正常显示的问题

--- kubejs/client_scripts/lang/zh_cn.js
+++ kubejs/client_scripts/lang/zh_cn.js
@@ -31,10 +31,11 @@ ClientEvents.lang("zh_cn", (event) => {
 	addMachineLanguaue("water_pump", "水泵")
 	addMachineLanguaue("flash_disk_writer", "闪存盘写入器")
 
-	addMBDMachineLanguaue("accelerator_energy_input", "大型构件催生器能量输入口", "")
-	addMBDMachineLanguaue("accelerator_item_input", "大型构件催生器物品输入口", "")
-	addMBDMachineLanguaue("accelerator_item_output", "大型构件催生器物品输出口", "")
-	addMBDMachineLanguaue("accelerator", "大型构件催生器控制器", "大型构件催生器")
+	addMBDMachineLanguaue("accelerator_energy_input", "大型构件催生器能量输入口")
+	addMBDMachineLanguaue("accelerator_item_input", "大型构件催生器物品输入口")
+	addMBDMachineLanguaue("accelerator_item_output", "大型构件催生器物品输出口")
+	addMBDMachineLanguaue("accelerator", "大型构件催生器控制器")
+	addMBDMachineGUILanguage("accelerator","大型构件催生器")
 
 	addMechanismPartLanguaue("basic", "基础")
 	addMechanismPartLanguaue("mechanical", "机械")
@@ -268,9 +269,11 @@ ClientEvents.lang("zh_cn", (event) => {
 		event.add(`machine.${global.namespace}.${key}`, value)
 	}
 
-	function addMBDMachineLanguaue(key, machine, recipe) {
-		event.add(`block.mbd2.${key}`, machine)
-		event.add(`mbd2.${key}`, recipe)
+	function addMBDMachineLanguaue(key, value) {
+		event.add(`block.mbd2.${key}`, value)
+	}
+	function addMBDMachineGUILanguage(key, value) {
+		event.add(`mbd2.${key}`, value)
 	}
 
 	function addFluidLanguaue(key, value) {
