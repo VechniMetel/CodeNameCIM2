cd49d7c8cc3b6280142ff4990fbb0297ee357430
Qi-Month <dy757410@163.com>
2025-05-06 21:24:00 +0800

    docs(ae2): remove ponder scenes and update config
    - Remove ponder scenes for AE2 cables, controller, drive and channels
    - Update recipe category sort order in JEI config
    - Adjust item tags and rarity in item registration

---
+++ kubejs/client_scripts/ponder/ae2/SystemChannels.js
@@ -0,0 +1,19 @@
+Ponder.registry((event) => {
+	event.create("ae2:fluix_glass_cable")
+		.tag("ue_addons:ae2")
+		.scene("ae2:channel", "频道", "ue_addons:ae2/standard_floor", (scene) => {
+			scene.showBasePlate()
+			scene.idle(20)
+
+			scene.text(40, "ae中频道是一个很重要的东西", [2, 1, 1])
+			scene.idle(60)
+			scene.text(40, "如果频道达到上限了，那么接下来放置的设备将无法接入me网络，无法实现自动化处理", [2, 1, 1])
+			scene.idle(60)
+			scene.text(40, "如果频道达到上限了，那么接下来放置的设备将无法接入me网络，无法实现自动化处理", [2, 1, 1])
+			scene.idle(60)
+			scene.text(40, "那么该如何避免这种情况呢？", [2, 1, 1])
+			scene.idle(60)
+			scene.text(40, "粗线缆有32个频道数，细线缆只有8个频道数", [2, 1, 1])
+			scene.idle(60)
+		})
+})
