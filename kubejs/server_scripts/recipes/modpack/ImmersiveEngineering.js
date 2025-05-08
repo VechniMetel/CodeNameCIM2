ca35895dc0740c016aa6f792fd0b73f61cc3d60c
Qi-Month <dy757410@163.com>
2025-05-08 22:37:05 +0800

    adjust recipes

--- kubejs/server_scripts/recipes/modpack/ImmersiveEngineering.js
+++ kubejs/server_scripts/recipes/modpack/ImmersiveEngineering.js
@@ -4,20 +4,29 @@ ServerEvents.recipes((event) => {
 		"type": "immersiveindustry:chemical",
 		"inputs": [
 			{
-				"base_ingredient": { "item": "ue_addons:blockstone_source_alpha" },
+				"base_ingredient": {
+					"item": "ue_addons:blockstone_source_alpha"
+				},
 				"count": 1
 			},
 			{
-				"base_ingredient": { "item": "ue_addons:blockstone_source_beta" },
+				"base_ingredient": {
+					"item": "ue_addons:blockstone_source_beta"
+				},
 				"count": 1
 			},
 			{
-				"base_ingredient": { "item": "ue_addons:blockstone_source_gamma" },
+				"base_ingredient": {
+					"item": "ue_addons:blockstone_source_gamma"
+				},
 				"count": 1
 			}
 		],
 		"result_fluids": [
-			{ "fluid": "ue_addons:delta_unstable_solution", "amount": 500 },
+			{
+				"fluid": "ue_addons:delta_unstable_solution",
+				"amount": 500
+			},
 		],
 		"time": 600
 	})
@@ -25,10 +34,21 @@ ServerEvents.recipes((event) => {
 	// 粗制铂微粒
 	event.custom({
 		"type": "immersiveindustry:electrolyzer",
-		"fluid": { "tag": "ue_addons:delta_unstable_solution", "amount": 1000 },
-		"result_fluid": { "fluid": "ue_addons:turbid_waste_liquid", "amount": 1000 },
-		"input": { "item": "ue_addons:compression_end_stone_dust_block" },
-		"result": { "item": "ue_addons:raw_aluminum_particles", "count": 2 },
+		"fluid": {
+			"tag": "ue_addons:delta_unstable_solution",
+			"amount": 1000
+		},
+		"result_fluid": {
+			"fluid": "ue_addons:turbid_waste_liquid",
+			"amount": 1000
+		},
+		"input": {
+			"item": "ue_addons:compression_end_stone_dust_block"
+		},
+		"result": {
+			"item": "ue_addons:raw_aluminum_particles",
+			"count": 2
+		},
 		"large_only": true,
 		"time": 600
 	})
@@ -37,34 +57,50 @@ ServerEvents.recipes((event) => {
 	event.custom({
 		"type": "immersiveengineering:mixer",
 		"energy": 10000,
-		"fluid": { "amount": 1000, "tag": "forge:glowstone" },
+		"fluid": {
+			"amount": 1000,
+			"tag": "forge:glowstone"
+		},
 		"inputs": [
-			{ "item": "ue_addons:aluminum_crystal_nucleus", "count": 4 },
-			{ "tag": "forge:dusts/end_stone", "count": 6 }
+			{
+				"base_ingredient": {
+					"item": "ue_addons:aluminum_crystal_nucleus"
+				},
+				"count": 4
+			},
+			{
+				"base_ingredient": {
+					"tag": "forge:dusts/redstone"
+				},
+				"count": 6
+			}
 		],
-		"result": { "amount": 500, "fluid": "ue_addons:platinum_solution" }
+		"result": {
+			"amount": 500,
+			"fluid": "ue_addons:platinum_solution"
+		}
 	})
 
 	// 熔融铂
 	event.custom({
 		"type": "immersivepetroleum:hydrotreater",
-		"energy": 12000,
+		"energy": 1200,
 		"input": {
-			"amount": 1000,
+			"amount": 100,
 			"tag": "ue_addons:platinum_solution"
 		},
 		"result": {
-			"amount": 2000,
+			"amount": 200,
 			"fluid": "tconstruct:molten_platinum"
 		},
 		"secondary_input": {
-			"amount": 1000,
+			"amount": 100,
 			"tag": "forge:redstone_acid"
 		},
 		"secondary_result": {
 			"chance": "0.02",
-			"item": "ue_addons:platinum_ingot"
+			"item": "ue_addons:platinum_nugget"
 		},
-		"time": 300
+		"time": 30
 	})
 })
--- kubejs/startup_scripts/register/fluid/Molten.js
+++ kubejs/startup_scripts/register/fluid/Molten.js
@@ -1,13 +1,30 @@
 StartupEvents.registry("fluid", (event) => {
-	let fluidRegisters = `${global.namespace}:block/fluid/metal/`
-
 	function addAloneMaterialFluid(name) {
 		event.create(`${global.namespace}:molten_${name}`)
-			.flowingTexture(`${fluidRegisters + name}_flow`)
-			.stillTexture(`${fluidRegisters + name}_still`)
+			.flowingTexture(`${global.namespace}:block/fluid/metal/flow`)
+			.stillTexture(`${global.namespace}:block/fluid/metal/still`)
 			.tag("forge:molten_materials")
 			.tag(`forge:molten_${name}`)
 			.tag(`tconstruct:molten_${name}`)
+
+		// 生成Json模型文件
+		let file = `kubejs/assets/${global.namespace}/models/item/molten_${name}_bucket.json`
+		JsonIO.write(file, {
+			"parent": "forge:item/bucket_drip",
+			"loader": "forge:fluid_container",
+		})
+
+		console.log(`molten_${name}已注册!`)
+	}
+
+	function addColorMaterialFluid(name, color) {
+		event.create(`${global.namespace}:molten_${name}`)
+			.flowingTexture(`${global.namespace}:block/fluid/metal/flow`)
+			.stillTexture(`${global.namespace}:block/fluid/metal/still`)
+			.thinTexture(color)
+			.bucketColor(color)
+			.tag("forge:molten_materials")
+			.tag(`forge:molten_${name}`)
 
 		// 生成Json模型文件
 		let file = `kubejs/assets/${global.namespace}/models/item/molten_${name}_bucket.json`
@@ -19,4 +36,6 @@ StartupEvents.registry("fluid", (event) => {
 
 		console.log(`molten_${name}已注册!`)
 	}
+
+	addColorMaterialFluid("source_sapphire", 0X0257F5)
 })
