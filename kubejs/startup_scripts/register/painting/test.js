f789b74d397e7eae59977f9aa88a22a9f0d8e2ed
Unknown_Entity_ <Unknown_Entity_@outlook.com>
2025-05-11 16:57:05 +0800

    secret paintings add

--- kubejs/startup_scripts/register/painting/test.js
+++ kubejs/startup_scripts/register/painting/test.js
@@ -1,6 +1,7 @@
 StartupEvents.registry("painting_variant", (event) => {
-	event.create(`${global.namespace}:test`)
+	event.create(`${global.namespace}:rr1`)
 		.width(16)
 		.height(16)
 		.tag("minecraft:placeable")
+		
 })
