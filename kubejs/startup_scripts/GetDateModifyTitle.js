6a663e1cab03b469d4ace8d8a8d6178584ab4e70
Qi-Month <dy757410@163.com>
2025-05-17 19:20:30 +0800

    Blacklist

--- kubejs/startup_scripts/GetDateModifyTitle.js
+++ kubejs/startup_scripts/GetDateModifyTitle.js
@@ -2,11 +2,14 @@
 
 let $LocalDateTime = Java.loadClass("java.time.LocalDateTime")
 
+let foolTitle = "Create: Infinity Mechanism"
+let defaultTitle = "Create: Mechanism and Innovation"
+
 StartupEvents.postInit((event) => {
 	let date = $LocalDateTime.now()
 	if (date.getMonthValue() === 4 && date.getDayOfMonth() === 1) {
-		Client.window.setTitle("Create: Infinity Mechanism")
+		Client.window.setTitle(foolTitle)
 	} else {
-		Client.window.setTitle("Create: Mechanism and Innovation")
+		Client.window.setTitle(defaultTitle)
 	}
 })
