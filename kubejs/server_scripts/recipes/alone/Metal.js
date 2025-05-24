11b1724d24be4b0759e21473c0dee1865a6bc244
RandomMechanism <oooooooo_zane@163.com>
2025-05-24 22:59:38 +0800

    再次扩充了金属类材料的配方代码
    添加了沉浸工程的配方
    修复了一个配方错误

--- kubejs/server_scripts/recipes/alone/Metal.js
+++ kubejs/server_scripts/recipes/alone/Metal.js
@@ -1,5 +1,5 @@
 ServerEvents.recipes((event) => {
-	let { minecraft, kubejs, create, createaddition, thermal } = event.recipes
+	let { minecraft, kubejs, create, createaddition, thermal, immersiveengineering } = event.recipes
 
 	global.metalGroup.forEach((metal) => {
 		if (!(Ingredient.of(`#forge:storage_blocks/${metal}`).itemIds.length === 0)) {
@@ -31,6 +31,18 @@ ServerEvents.recipes((event) => {
 		if (!(Ingredient.of(`#forge:ores/${metal}`).itemIds.length === 0)) {
 			minecraft.smelting(`#forge:ingots/${metal}`, `#forge:ores/${metal}`)
 			minecraft.blasting(`#forge:ingots/${metal}`, `#forge:ores/${metal}`)
+			thermal.smelter([
+				`2x #forge:ingots/${metal}`,
+				Item.of("thermal:rich_slag").withChance(0.2)
+			], `#forge:ores/${metal}`)
+			immersiveengineering.arc_furnace(`2x #forge:ingots/${metal}`)
+				.input(`#forge:ores/${metal}`)
+				.slag("immersiveengineering:slag")
+				.time(200)
+				.energy(102400)
+				.additives([])
+		} else {
+			console.warn(`No ore found for ${metal}!`)
 		}
 
 		if (!(Ingredient.of(`#forge:raw_materials/${metal}`).itemIds.length === 0)) {
@@ -48,12 +60,28 @@ ServerEvents.recipes((event) => {
 			}
 			minecraft.smelting(`#forge:ingots/${metal}`, `#forge:raw_materials/${metal}`)
 			minecraft.blasting(`#forge:ingots/${metal}`, `#forge:raw_materials/${metal}`)
-			thermal.smelter([
-				`#forge:ingots/${metal}`,
-				Item.of(Ingredient.of(`#forge:ingots/${metal}`).itemIds[0]).withChance(0.5)
-			], `#forge:raw_materials/${metal}`)
+			thermal.smelter(
+				Item.of(Ingredient.of(`#forge:ingots/${metal}`).itemIds[0]).withChance(1.5)
+			, `#forge:raw_materials/${metal}`)
+			immersiveengineering.arc_furnace(`#forge:ingots/${metal}`)
+			.secondaries([Item.of(Ingredient.of(`#forge:ingots/${metal}`).itemIds[0]).withChance(0.5)])
+			.input(`#forge:raw_materials/${metal}`)
+			.slag("immersiveengineering:slag")
+			.time(900)
+			.energy(230400)
+			.additives([])
 		} else {
 			console.warn(`No raw material found for ${metal}!`)
+		}
+
+		if (!(Ingredient.of(`#forge:storage_blocks/raw_${metal}`).itemIds.length === 0)) {
+			immersiveengineering.arc_furnace(`13x #forge:ingots/${metal}`)
+			.secondaries(Item.of(Ingredient.of(`#forge:ingots/${metal}`).itemIds[0]).withChance(0.5))
+			.input(`#forge:storage_blocks/raw_${metal}`)
+			.energy(25600)
+			.additives([])
+		} else {
+			console.warn(`No storage block found for raw ${metal}!`)
 		}
 
 		if (!(Ingredient.of(`#create:crushed_raw_materials/${metal}`).itemIds.length === 0)) {
@@ -90,34 +118,53 @@ ServerEvents.recipes((event) => {
 			console.warn(`No crushed raw material found for ${metal}!`)
 		}
 
+		if (!(Ingredient.of(`#forge:dusts/${metal}`).itemIds.length === 0)) {
+			minecraft.smelting(`#forge:ingots/${metal}`, `#forge:dusts/${metal}`)
+			minecraft.blasting(`#forge:ingots/${metal}`, `#forge:dusts/${metal}`)
+			thermal.pulverizer(`#forge:dusts/${metal}`, `#forge:ingots/${metal}`).energy(2000)
+			thermal.smelter(`#forge:ingots/${metal}`, `#forge:dusts/${metal}`).energy(1600)
+			immersiveengineering.crusher(`#forge:dusts/${metal}`)
+				.secondaries([])
+				.input(`#forge:ingots/${metal}`)
+			immersiveengineering.arc_furnace(`forge:ingots/${metal}`)
+				.secondaries([])
+				.input(`#forge:dusts/${metal}`)
+				.additives([])
+			if (!(Ingredient.of(`#forge:raw_materials/${metal}`).itemIds.length === 0)) {
+				thermal.pulverizer(
+					Item.of(Ingredient.of(`#forge:dusts/${metal}`).itemIds[0]).withChance(1.25)
+					, `#forge:raw_materials/${metal}`
+				)
+				immersiveengineering.crusher(`#forge:dusts/${metal}`, `#forge:raw_materials/${metal}`)
+					.secondaries(Item.of(Ingredient.of(`#forge:dusts/${metal}`).itemIds[0]).withChance(1/3))
+			} else {
+				console.warn(`No raw material found for ${metal}!`)
+			}
+			if (!(Ingredient.of(`#forge:ores/${metal}`).itemIds.length === 0)) {
+				thermal.pulverizer(`2x #forge:dusts/${metal}`, `#forge:ores/${metal}`)
+				immersiveengineering.crusher(`2x #forge:dusts/${metal}`)
+					.secondaries([])
+					.input(`#forge:ores/${metal}`)
+			} else {
+				console.warn(`No ore found for ${metal}!`)
+			}
+			if (!(Ingredient.of(`#forge:storage_blocks/raw_${metal}`).itemIds.length === 0)) {
+				immersiveengineering.crusher(`12x #forge:dusts/${metal}`)
+					.secondaries([])
+					.input(`#forge:storage_blocks/raw_${metal}`)
+			} else {
+				console.warn(`No storage block found for raw ${metal}!`)
+			}
+		} else {
+			console.warn(`No dust found for ${metal}!`)
+		}
+
 		if (!(Ingredient.of(`#forge:plates/${metal}`).itemIds.length === 0)) {
 			create.pressing(`#forge:plates/${metal}`, `#forge:ingots/${metal}`)
 			thermal.press(`#forge:plates/${metal}`, `#forge:ingots/${metal}`)
 			thermal.smelter(`#forge:ingots/${metal}`, `#forge:plates/${metal}`).energy(1600)
 		} else {
 			console.warn(`No plate found for ${metal}!`)
-		}
-
-		if (!(Ingredient.of(`#forge:dusts/${metal}`).itemIds.length === 0)) {
-			minecraft.smelting(`#forge:ingots/${metal}`, `#forge:dusts/${metal}`)
-			minecraft.blasting(`#forge:ingots/${metal}`, `#forge:dusts/${metal}`)
-			thermal.pulverizer(`#forge:dusts/${metal}`, `#forge:ingots/${metal}`).energy(2000)
-			thermal.smelter(`#forge:ingots/${metal}`, `#forge:dusts/${metal}`).energy(1600)
-			if (!(Ingredient.of(`#forge:raw_materials/${metal}`).itemIds.length === 0)) {
-				thermal.pulverizer([
-					`#forge:dusts/${metal}`,
-					Item.of(Ingredient.of(`#forge:dusts/${metal}`).itemIds[0]).withChance(0.25)
-				], `#forge:raw_materials/${metal}`)
-			} else {
-				console.warn(`No raw material found for ${metal}!`)
-			}
-			if (!(Ingredient.of(`#forge:ores/${metal}`).itemIds.length === 0)) {
-				thermal.pulverizer(`2x #forge:dusts/${metal}`, `#forge:ores/${metal}`)
-			} else {
-				console.warn(`No ore found for ${metal}!`)
-			}
-		} else {
-			console.warn(`No dust found for ${metal}!`)
 		}
 
 		if (!(Ingredient.of(`#forge:rods/${metal}`).itemIds.length === 0)) {
