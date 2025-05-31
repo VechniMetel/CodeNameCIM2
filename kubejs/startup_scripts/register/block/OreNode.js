1ccfdba61f5d12d3a70408af58827e778d2e449e
117458866249 <yang33444433@icloud.com>
2025-05-31 17:39:08 +0800

    Add files via upload

---
+++ kubejs/startup_scripts/register/block/OreNode.js
@@ -0,0 +1,45 @@
+StartupEvents.registry("block", (event) => {
+	function addOreNode(name, types) {
+		const TYPE_LIST = Array.isArray(types) ? types : [types]
+
+		TYPE_LIST.forEach((type) => {
+			if (type !== "nether" && type !== "deepslate") {
+				event.create(`${global.namespace}:${type}_${name}_ore_node`)
+					.soundType(SoundType.STONE)
+					.hardness(-1)
+					.resistance(100)
+					.textureAll(`${global.namespace}:block/ore_node/${name}/${type}`)
+					.tagItem(`${global.namespace}:ore_nodes`)
+					.tag("deepdrilling:ore_nodes")
+			} else if (type === "deepslate") {
+				event.create(`${global.namespace}:${type}_${name}_ore_node`)
+					.soundType(SoundType.DEEPSLATE)
+					.hardness(-1)
+					.resistance(100)
+					.textureAll(`${global.namespace}:block/ore_node/${name}/${type}`)
+					.tagItem(`${global.namespace}:ore_nodes`)
+					.tag("deepdrilling:ore_nodes")
+			} else if (type === "nether") {
+				event.create(`${global.namespace}:${type}_${name}_ore_node`)
+					.soundType(SoundType.NETHER_ORE)
+					.hardness(-1)
+					.resistance(100)
+					.textureAll(`${global.namespace}:block/ore_node/${name}/${type}`)
+					.tagItem(`${global.namespace}:ore_nodes`)
+					.tag("deepdrilling:ore_nodes")
+			}
+		})
+	}
+
+	addOreNode("uranium", "nether")
+	addOreNode("copper", "deepslate")
+	addOreNode("iron", "deepslate")
+	addOreNode("zinc", "deepslate")
+	addOreNode("gold", "deepslate")
+	addOreNode("platinum", "moon")
+	addOreNode("cheese", "moon")
+
+	// 新支持的数组调用方式示例
+	// addOreNode("diamond", ["deepslate", "nether"])
+	// addOreNode("emerald", ["stone", "deepslate"])
+})
