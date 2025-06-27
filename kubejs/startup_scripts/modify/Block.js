c316231d856abf1471585b8d878f7d5f3d648456
Qi-Month <dy757410@163.com>
2025-06-27 21:04:22 +0800

    Water pump SoundType

--- kubejs/startup_scripts/modify/Block.js
+++ kubejs/startup_scripts/modify/Block.js
@@ -2,4 +2,8 @@ BlockEvents.modification((event) => {
 	event.modify("mekanism:cardboard_box", (modify) => {
 		modify.soundType = SoundType.NETHERITE_BLOCK
 	})
+
+	event.modify("cmi:water_pump", (modify) => {
+		modify.soundType = SoundType.WOOD
+	})
 })
