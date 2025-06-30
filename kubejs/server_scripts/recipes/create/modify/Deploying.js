38f51f4e28104407c6c3e33a2ff5a9db5b261f87
Unknown_Entity_ <Unknown_Entity_@outlook.com>
2025-06-30 19:00:20 +0800

    ie metal plate and block&conpon.... fix

--- kubejs/server_scripts/recipes/create/modify/Deploying.js
+++ kubejs/server_scripts/recipes/create/modify/Deploying.js
@@ -2,7 +2,12 @@ ServerEvents.recipes((event) => {
 	let { create } = event.recipes
 
 	create.deploying("immersiveengineering:component_iron", [
-		"#forge:rods/copper",
-		"immersiveengineering:sheetmetal_iron"
+		"immersiveengineering:sheetmetal_iron",
+		"#forge:rods/copper"
+	])
+
+	create.deploying("immersiveengineering:component_steel", [
+		"immersiveengineering:sheetmetal_steel",
+		"#forge:rods/copper"
 	])
 })
--- kubejs/server_scripts/recipes/immersiveengineering/Metal.js
+++ kubejs/server_scripts/recipes/immersiveengineering/Metal.js
@@ -6,4 +6,81 @@ ServerEvents.recipes((event) => {
     	event.recipes.stonecutting( "#forge:plates/iron",
 		'immersiveengineering:sheetmetal_iron'
 	)
+
+	event.recipes.stonecutting('immersiveengineering:sheetmetal_copper' ,
+		"#forge:plates/copper"
+	),
+    	event.recipes.stonecutting( "#forge:plates/copper",
+		'immersiveengineering:sheetmetal_copper'
+	)
+
+	event.recipes.stonecutting('immersiveengineering:sheetmetal_steel' ,
+		"#forge:plates/steel"
+	),
+    	event.recipes.stonecutting( "#forge:plates/steel",
+		'immersiveengineering:sheetmetal_steel'
+	)
+
+	event.recipes.stonecutting('immersiveengineering:sheetmetal_aluminum' ,
+		"#forge:plates/aluminum"
+	),
+    	event.recipes.stonecutting( "#forge:plates/aluminum",
+		'immersiveengineering:sheetmetal_aluminum'
+	)
+
+	event.recipes.stonecutting('immersiveengineering:sheetmetal_lead' ,
+		"#forge:plates/lead"
+	),
+    	event.recipes.stonecutting( "#forge:plates/lead",
+		'immersiveengineering:sheetmetal_lead'
+	)
+
+	event.recipes.stonecutting('immersiveengineering:sheetmetal_silver' ,
+		"#forge:plates/silver"
+	),
+    	event.recipes.stonecutting( "#forge:plates/silver",
+		'immersiveengineering:sheetmetal_silver'
+	)
+
+	event.recipes.stonecutting('immersiveengineering:sheetmetal_uranium' ,
+		"#forge:plates/uranium"
+	),
+    	event.recipes.stonecutting( "#forge:plates/uranium",
+		'immersiveengineering:sheetmetal_uranium'
+	)
+
+	event.recipes.stonecutting('immersiveengineering:sheetmetal_nickel' ,
+		"#forge:plates/nickel"
+	),
+    	event.recipes.stonecutting( "#forge:plates/nickel",
+		'immersiveengineering:sheetmetal_nickel'
+	)
+
+	event.recipes.stonecutting('immersiveengineering:sheetmetal_constantan' ,
+		"#forge:plates/constantan"
+	),
+    	event.recipes.stonecutting( "#forge:plates/constantan",
+		'immersiveengineering:sheetmetal_constantan'
+	)
+
+	event.recipes.stonecutting('immersiveengineering:sheetmetal_electrum' ,
+		"#forge:plates/electrum"
+	),
+    	event.recipes.stonecutting( "#forge:plates/electrum",
+		'immersiveengineering:sheetmetal_electrum'
+	)
+
+	event.recipes.stonecutting('immersiveengineering:sheetmetal_steel' ,
+		"#forge:plates/steel"
+	),
+    	event.recipes.stonecutting( "#forge:plates/steel",
+		'immersiveengineering:sheetmetal_steel'
+	)
+
+	event.recipes.stonecutting('immersiveengineering:sheetmetal_gold' ,
+		"#forge:plates/gold"
+	),
+    	event.recipes.stonecutting( "#forge:plates/gold",
+		'immersiveengineering:sheetmetal_gold'
+	)
 })
