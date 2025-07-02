349b3c657f4cb7bb54a561a82f80ee417a3b0842
Qi-Month <dy757410@163.com>
2025-07-02 22:18:05 +0800

    ts

--- kubejs/probe/docs/metal_material.d.ts
+++ kubejs/probe/docs/metal_material.d.ts
@@ -10,8 +10,6 @@ type Color = number;
 type ResourceLocation = string;
 type Tag = string;
 type ItemStack = any;
-type Fluid = string;
-type FluidTag = string;
 type SoundType = string;
 
 // 材料类型定义
@@ -35,13 +33,6 @@ interface Material {
 	block(): this;
 	molten(): this;
 }
-
-// 文件操作接口
-interface JsonIO {
-	write(filePath: string, data: object): void;
-}
-
-declare const JsonIO: JsonIO
 
 // 材料管理函数
 type RegisterMetalMaterial = (name: string, color: Color, level: MiningLevel) => Material;
