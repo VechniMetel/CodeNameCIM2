81dfe54720387e44b11cf34e9aaf9f88ab9b4175
117458866249 <yang33444433@icloud.com>
2025-07-04 21:50:10 +0800

    机械方块

--- kubejs/server_scripts/recipes/modpack/MachineBlock.js
+++ kubejs/server_scripts/recipes/modpack/MachineBlock.js
@@ -1,43 +1,43 @@
 ServerEvents.recipes(event => {
     let { kubejs } = event.recipes
-    kubejs.shaped('cmi:andesite_machine', [
+    kubejs.shaped("cmi:andesite_machine", [
         " A ",
         " B ",
         "   "
     ], {
-        A: 'cmi:andesite_mechanism',
-        B: 'create:andesite_casing'
+        A: "cmi:andesite_mechanism",
+        B: "create:andesite_casing"
     })
-    kubejs.shaped('cmi:brass_machine', [
+    kubejs.shaped("cmi:brass_machine", [
         " A ",
         " B ",
         "   "
     ], {
-        A: 'create:precision_mechanism',
-        B: 'create:brass_casing'
+        A: "create:precision_mechanism",
+        B: "create:brass_casing"
     })
-    kubejs.shaped('cmi:copper_machine', [
+    kubejs.shaped("cmi:copper_machine", [
         " A ",
         " B ",
         "   "
     ], {
-        A: 'cmi:copper_mechanism',
-        B: 'create:copper_casing'
+        A: "cmi:copper_mechanism",
+        B: "create:copper_casing"
     })
-    kubejs.shaped('cmi:me_machine', [
+    kubejs.shaped("cmi:me_machine", [
         " A ",
         " B ",
         "   "
     ], {
-        A: 'cmi:smart_mechanism',
-        B: 'ae2:quartz_glass'
+        A: "cmi:smart_mechanism",
+        B: "ae2:quartz_glass"
     })
-    kubejs.shaped('cmi:mekanism_machine', [
+    kubejs.shaped("cmi:mekanism_machine", [
         " A ",
         " B ",
         "   "
     ], {
-        A: 'cmi:basic_mekanism_mechanism',
-        B: 'mekanism:steel_casing'
+        A: "cmi:basic_mekanism_mechanism",
+        B: "mekanism:steel_casing"
     })
 })
