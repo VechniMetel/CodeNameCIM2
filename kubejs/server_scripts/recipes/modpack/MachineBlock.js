e53b44879c5898b70d2160ab21d5962a88fd827f
117458866249 <yang33444433@icloud.com>
2025-07-04 21:47:19 +0800

    Add files via upload

---
+++ kubejs/server_scripts/recipes/modpack/MachineBlock.js
@@ -0,0 +1,43 @@
+ServerEvents.recipes(event => {
+    let { kubejs } = event.recipes
+    kubejs.shaped('cmi:andesite_machine', [
+        " A ",
+        " B ",
+        "   "
+    ], {
+        A: 'cmi:andesite_mechanism',
+        B: 'create:andesite_casing'
+    })
+    kubejs.shaped('cmi:brass_machine', [
+        " A ",
+        " B ",
+        "   "
+    ], {
+        A: 'create:precision_mechanism',
+        B: 'create:brass_casing'
+    })
+    kubejs.shaped('cmi:copper_machine', [
+        " A ",
+        " B ",
+        "   "
+    ], {
+        A: 'cmi:copper_mechanism',
+        B: 'create:copper_casing'
+    })
+    kubejs.shaped('cmi:me_machine', [
+        " A ",
+        " B ",
+        "   "
+    ], {
+        A: 'cmi:smart_mechanism',
+        B: 'ae2:quartz_glass'
+    })
+    kubejs.shaped('cmi:mekanism_machine', [
+        " A ",
+        " B ",
+        "   "
+    ], {
+        A: 'cmi:basic_mekanism_mechanism',
+        B: 'mekanism:steel_casing'
+    })
+})
