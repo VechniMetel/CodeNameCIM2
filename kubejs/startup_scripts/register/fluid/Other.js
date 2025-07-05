b495032141b692aa14090f258e2aea91f76654b8
Qi-Month <dy757410@163.com>
2025-07-05 14:37:05 +0800

    fix fluid registers

---
+++ kubejs/startup_scripts/register/fluid/Other.js
@@ -0,0 +1,39 @@
+StartupEvents.registry("fluid", (event) => {
+	function addAloneFluid(name) {
+		event.create(`${global.namespace}:${name}`)
+			.flowingTexture(`${global.namespace}:block/fluid/${name}/flow`)
+			.stillTexture(`${global.namespace}:block/fluid/${name}/still`)
+
+		let file = `kubejs/assets/${global.namespace}/models/item/${name}_bucket.json`
+		JsonIO.write(file, {
+			"parent": "forge:item/bucket_drip",
+			"loader": "forge:fluid_container",
+			"fluid": `${global.namespace}:${name}`
+		})
+
+		console.log(`${global.namespace}:${name}已注册!`)
+	}
+
+	function addColorFluid(name, color) {
+		event.create(`${global.namespace}:${name}`)
+			.thinTexture(color)
+			.bucketColor(color)
+			.flowingTexture(`${global.namespace}:block/fluid/solution/flow`)
+			.stillTexture(`${global.namespace}:block/fluid/solution/still`)
+
+		let file = `kubejs/assets/${global.namespace}/models/item/${name}_bucket.json`
+		JsonIO.write(file, {
+			"parent": "forge:item/bucket_drip",
+			"loader": "forge:fluid_container",
+			"fluid": `${global.namespace}:${name}`
+		})
+	}
+
+	addColorFluid("eletriced_source_emeraid", 0X117458)
+	addColorFluid("platinum_solution", 0xE1FFFF)
+
+	addAloneFluid("blood")
+	addAloneFluid("delta_unstable_solution")
+	addAloneFluid("turbid_waste_liquid")
+	addAloneFluid("pure_sand")
+})
