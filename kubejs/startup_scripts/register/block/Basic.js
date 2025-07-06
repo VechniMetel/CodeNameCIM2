ff5d3eaa25aec9ff268d720c5ffae9b071c6b810
Unknown_Entity_ <Unknown_Entity_@outlook.com>
2025-07-06 15:04:25 +0800

    player model

--- kubejs/startup_scripts/register/block/Basic.js
+++ kubejs/startup_scripts/register/block/Basic.js
@@ -6,14 +6,46 @@ StartupEvents.registry("block", (event) => {
 		.requiresTool(true)
 
 	let machineBlockRegister = [
-		"andesite",
-		"brass",
+		"nature",
+		"wooden",
+		"stone",
+		"iron",
 		"copper",
-		"me",
-		"mekanism"
+		"andesite",
+		"photosensitive",
+		"gold",
+		"cobalt",
+		"nether",
+		"thermal",
+		"feinforced",
+		"resonant",
+		"railway",
+		"ender",
+		"light_engineering",
+		"heavy_engineering",
+		"enchanted",
+		"smart",
+		"computing",
+		"tier_1_aviation",
+		"tier_2_aviation",
+		"tier_3_aviation",
+		"tier_4_aviation",
+		"basic_mekanism",
+		"advanced_mekanism",
+		"elite_mekanism",
+		"ultimate_mekanism",
+		"structure",
+		"antimatter",
+		"coil",
+		"sculk",
+		"colorful",
+		"creative",
+		"potion",
+		"precision",
+		"redstone"
 	]
 	machineBlockRegister.forEach((type) => {
-		event.create(`${global.namespace}:${type}_machine`, "cardinal")
+		event.create(`${global.namespace}:${type}_machine`,"cardinal")
 			.soundType(SoundType.METAL)
 			.tagBlock(global.toolType["pickaxe"])
 			.tagBlock(global.miningLevel["wooden"])
