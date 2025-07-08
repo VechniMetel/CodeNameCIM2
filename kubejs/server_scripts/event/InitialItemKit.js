ce0006e4737a6456ae34e527623df2eba9227f8e
Qi-Month <dy757410@163.com>
2025-07-08 20:53:38 +0800

    Del TagsPush.js

--- kubejs/server_scripts/event/InitialItemKit.js
+++ kubejs/server_scripts/event/InitialItemKit.js
@@ -2,7 +2,7 @@ ItemEvents.rightClicked((event) => {
 	let { player, item } = event
 	const KIT = "cmi:initial_item_kit"
 
-	const ITEM_LIST = [
+	let list = [
 		"create:wrench",
 		"create:goggles",
 		"create:super_glue"
@@ -11,7 +11,7 @@ ItemEvents.rightClicked((event) => {
 	if (player.mainHandItem === KIT && player.offHandItem !== KIT && player.crouching) {
 		let key = `message.${global.namespace}.initial_item_kit`
 		player.tell(Component.translate(key))
-		ITEM_LIST.forEach((item) => {
+		list.forEach((item) => {
 			player.give(item)
 		})
 		item.shrink(1)
