53ff11c5ec588e912c96d48930ebf4bc5b85f417
Unknown_Entity_ <Unknown_Entity_@outlook.com>
2025-07-08 19:28:12 +0800

    复位1928

--- kubejs/startup_scripts/register/block/Basic.js
+++ kubejs/startup_scripts/register/block/Basic.js
@@ -90,6 +90,4 @@ StartupEvents.registry("block", (event) => {
 		.tagBlock(global.toolType["pickaxe"])
 		.tagBlock(global.miningLevel["iron"])
 		.requiresTool(true)
-
-	event.create(`${global.namespace}:ue_1922`)
 })
