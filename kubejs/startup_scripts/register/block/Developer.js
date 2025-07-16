f61e23c407953bf81b47a611e5428f63fd5a82f5
Qi-Month <dy757410@163.com>
2025-07-16 17:59:18 +0800

    code Optimization

---
+++ kubejs/startup_scripts/register/block/Developer.js
@@ -0,0 +1,27 @@
+StartupEvents.registry("block", (event) => {
+	function addDeveloperDoll(name) {
+		event.create(`${global.namespace}:${name}`, "cardinal")
+			.soundType(SoundType.WOOL)
+			.hardness(0)
+			.resistance(1)
+			.requiresTool(true)
+			.model(`${global.namespace}:block/dev/${name}`)
+			.textureAll(`${global.namespace}:block/dev/${name}`)
+			.notSolid()
+			.defaultCutout()
+			.notSolid()
+			.noCollision()
+	}
+	// UE
+	addDeveloperDoll("unknown_entity_")
+	// 柒月
+	addDeveloperDoll("qi_month")
+	// 中微子
+	addDeveloperDoll("vechni_metel")
+	// 1174
+	addDeveloperDoll("117458866249")
+	// Aleegos
+	addDeveloperDoll("aleegos")
+	// FiberOptics
+	addDeveloperDoll("fiber_optics")
+})
