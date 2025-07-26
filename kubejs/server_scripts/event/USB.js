0efbe5cc0bba449d58cc53632be2d8c4693ac2ac
117458866249 <yang33444433@icloud.com>
2025-07-26 10:09:54 +0800

    Flash drive Pick Up & Models(unfinished)

--- kubejs/server_scripts/event/USB.js
+++ kubejs/server_scripts/event/USB.js
@@ -1,57 +1,66 @@
 //构件闪存盘
-BlockEvents.rightClicked(event => {
+BlockEvents.rightClicked((event) => {
     const { player, item, block } = event;
+    let { x, y, z } = block
     //定义构件有哪些
     let mechanismRegister = [
-            "nature",
-            "wooden",
-            "stone",
-            "iron",
-            "copper",
-            "andesite",
-            "photosensitive",
-            "gold",
-            "cobalt",
-            "nether",
-            "thermal",
-            "feinforced",
-            "resonant",
-            "railway",
-            "ender",
-            "light_engineering",
-            "heavy_engineering",
-            "enchanted",
-            "smart",
-            "computing",
-            "tier_1_aviation",
-            "tier_2_aviation",
-            "tier_3_aviation",
-            "tier_4_aviation",
-            "basic_mekanism",
-            "advanced_mekanism",
-            "elite_mekanism",
-            "ultimate_mekanism",
-            "structure",
-            "nuclear",
-            "antimatter",
-            "coil",
-            "sculk",
-            "colorful",
-            "creative",
-            "pigiron",
-            "potion",
-            "bronze",
-            "precision",
-            "redstone"
-        ]
-        //给每个构件测试是不是手上构件方块USB插口
+        "nature",
+        "wooden",
+        "stone",
+        "iron",
+        "copper",
+        "andesite",
+        "photosensitive",
+        "gold",
+        "cobalt",
+        "nether",
+        "thermal",
+        "feinforced",
+        "resonant",
+        "railway",
+        "ender",
+        "light_engineering",
+        "heavy_engineering",
+        "enchanted",
+        "smart",
+        "computing",
+        "tier_1_aviation",
+        "tier_2_aviation",
+        "tier_3_aviation",
+        "tier_4_aviation",
+        "basic_mekanism",
+        "advanced_mekanism",
+        "elite_mekanism",
+        "ultimate_mekanism",
+        "structure",
+        "nuclear",
+        "antimatter",
+        "coil",
+        "sculk",
+        "colorful",
+        "creative",
+        "pigiron",
+        "potion",
+        "bronze",
+        "precision",
+        "redstone"
+    ]
+    //给每个构件测试是不是手上构件方块USB插口
     mechanismRegister.forEach((material) => {
-        if (item.id === `cmi:${material}_mechanism_flash_drive` && block.id === "cmi:usb_port") {
+        if (item.id === `cmi:${material}_mechanism_flash_drive` && block.id === "cmi:usb_port" && player.crouching === false) {
             block.set(`cmi:usb_port_${material}`);
             if (!player.isCreative()) {
-                // 若是生存模式则减少一个构件
+                // 若是生存模式则减少一个构件闪存盘
                 event.getItem().shrink(1)
             }
+        }
+    })
+    //闪存盘的拔出
+    mechanismRegister.forEach((material) => {
+        if (item.id === "minecraft:air" && block.id === `cmi:usb_port_${material}` && player.crouching === true) {
+            block.set("cmi:usb_port");
+            let command = `execute as ${player.username} at ${player.username} positioned as ${player.username} run give ${player.username} cmi:${material}_mechanism_flash_drive`
+            player.runCommandSilent(command)
         }
     })
 });
