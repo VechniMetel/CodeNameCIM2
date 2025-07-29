1f05cc26b389979659425d86d3b20c92fde0e320
117458866249 <yang33444433@icloud.com>
2025-07-29 17:52:50 +0800

    Stone & Iron Mechanism

---
+++ kubejs/client_scripts/ponder/dev_ftb/Background.js
@@ -0,0 +1,111 @@
+Ponder.registry((event) => {
+	event.create("cmi:green_screen")
+		.scene("1", "1", "cmi:dev_ftb/ftb_wooden", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.2)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_stone", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.5)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_iron", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.3)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_andesite", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.3)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_copper", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.2)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_gold", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.4)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_nether", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.2)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_photosensitive", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.3)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_railway", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.3)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_precision", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.2)
+        })
+		.scene("1", "1", "cmi:dev_ftb/ftb_thermal", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.4)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_cobalt", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.2)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_pigiron", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.3)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_nature", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.3)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_enchanted", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.3)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_feinforced", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.2)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_ender", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.3)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_resonant", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.3)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_light_ie", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.3)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_heavy_ie", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.3)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_smart", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.2)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_computing", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.2)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_cobalt", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.2)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_sculk", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.3)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_colorful", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.3)
+		})
+		.scene("1", "1", "cmi:dev_ftb/ftb_coil", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.3)
+		})
+		.scene("2", "2", "cmi:dev_ftb/ftb_tier1", (scene) => {
+			scene.showStructure()
+			scene.scaleSceneView(0.3)
+		})
+})
