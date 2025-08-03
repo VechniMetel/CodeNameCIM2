4b4e8bf889fa0f74d541497e353d443ef3d1a8d9
Qi-Month <dy757410@163.com>
2025-08-03 09:37:05 +0800

    纹理

--- kubejs/startup_scripts/register/item/Incomplete.js
+++ kubejs/startup_scripts/register/item/Incomplete.js
@@ -1,11 +1,11 @@
 StartupEvents.registry("item", (event) => {
     // mek未完成电路板
 	event.create(`${global.namespace}:incomplete_advanced_control_circuit`)
-		.texture(`${global.namespace}:item/advanced_control_circuit`)
+		.texture(`${global.namespace}:item/material/advanced_control_circuit`)
 
 	event.create(`${global.namespace}:incomplete_elite_control_circuit`)
-		.texture(`${global.namespace}:item/elite_control_circuit`)
+		.texture(`${global.namespace}:item/material/elite_control_circuit`)
 
 	event.create(`${global.namespace}:incomplete_ultimate_control_circuit`)
-		.texture(`${global.namespace}:item/ultimate_control_circuit`)
+		.texture(`${global.namespace}:item/material/ultimate_control_circuit`)
 })
