87a75b6bd01346f6cc228e39ba6b1f519a5ab392
Qi-Month <dy757410@163.com>
2025-08-06 10:43:08 +0800

    builder

--- kubejs/startup_scripts/register/block/RawBlocks.js
+++ kubejs/startup_scripts/register/block/RawBlocks.js
@@ -1,15 +1,18 @@
 StartupEvents.registry("block", (event) => {
 	function addAloneRawMaterialBlock(name, level) {
-		return event.create(`${global.namespace}:raw_${name}_block`)
-			.soundType(SoundType.STONE)
-			.hardness(5)
-			.resistance(5)
-			.textureAll(`${global.namespace}:block/material/raw/${name}`)
-			.tagBlock(global.toolType["pickaxe"])
-			.tagBlock(global.miningLevel[level])
-			.tagItem(`${global.namespace}:raw_metals`)
-			.tag("forge:storage_blocks")
-			.tag(`forge:storage_blocks/raw_${name}`)
+		let registerAloneRawMaterial =
+			event.create(`${global.namespace}:raw_${name}_block`)
+				.soundType(SoundType.STONE)
+				.hardness(5)
+				.resistance(5)
+				.textureAll(`${global.namespace}:block/material/raw/${name}`)
+				.tagBlock(global.toolType["pickaxe"])
+				.tagBlock(global.miningLevel[level])
+				.tagItem(`${global.namespace}:raw_metals`)
+				.tag("forge:storage_blocks")
+				.tag(`forge:storage_blocks/raw_${name}`)
+
+		return registerAloneRawMaterial
 	}
 
 	addAloneRawMaterialBlock("vanadium", "iron")
