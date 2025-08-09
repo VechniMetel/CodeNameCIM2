069d0edb2cd989ef3523832e760db1651682737d
Qi-Month <dy757410@163.com>
2025-08-09 18:25:27 +0800

    TConTool

--- kubejs/startup_scripts/register/item/TConTool.js
+++ kubejs/startup_scripts/register/item/TConTool.js
@@ -8,31 +8,36 @@ let $TinkerTags$Items =
 	Java.loadClass("slimeknights.tconstruct.common.TinkerTags$Items")
 
 StartupEvents.registry("item", (event) => {
-	const ITEM_PRO = new $Item$Properties()
-		.stacksTo(1)
-	const PAXEL = $ToolDefinition.create(`${global.namespace}:paxel`)
-
-	function addTConTool(name, tool) {
-		return event.createCustom(name, () => {
-			return new $ModifiableItem(ITEM_PRO, tool)
-				.tag("forge:tools")
-				.tag("minecraft:tools")
+	let TinkerItemTags = {}
+	Object.keys($TinkerTags$Items)
+		.forEach((key) => {
+			let value = $TinkerTags$Items[key]
+			if (value && typeof value.location === "function") {
+				TinkerItemTags[key] = value.location().toString()
+			}
 		})
+	function addTConToolType(name) {
+		return $ToolDefinition.create(`${global.namespace}:${name}`)
+	}
+	function addTConTool(name) {
+		return event.createCustom(`${global.namespace}:${name}`, () => {
+			return new $ModifiableItem(new $Item$Properties(), addTConToolType(name))
+		}).tag("forge:tools").tag("minecraft:tools")
 	}
 
-	addTConTool("paxel", PAXEL)
+	addTConTool("paxel")
 		.tag("minecraft:pickaxes")
 		.tag("minecraft:axes")
 		.tag("minecraft:shovels")
-		.tag($TinkerTags$Items.MODIFIABLE)
-		.tag($TinkerTags$Items.MULTIPART_TOOL)
-		.tag($TinkerTags$Items.DURABILITY)
-		.tag($TinkerTags$Items.SMALL_TOOLS)
-		.tag($TinkerTags$Items.MELEE)
-		.tag($TinkerTags$Items.HELD)
-		.tag($TinkerTags$Items.HARVEST)
-		.tag($TinkerTags$Items.HARVEST_PRIMARY)
-		.tag($TinkerTags$Items.STONE_HARVEST)
-		.tag($TinkerTags$Items.INTERACTABLE)
-		.tag($TinkerTags$Items.INTERACTABLE_RIGHT)
+		.tag(TinkerItemTags.MODIFIABLE)
+		.tag(TinkerItemTags.MULTIPART_TOOL)
+		.tag(TinkerItemTags.DURABILITY)
+		.tag(TinkerItemTags.SMALL_TOOLS)
+		.tag(TinkerItemTags.MELEE)
+		.tag(TinkerItemTags.HELD)
+		.tag(TinkerItemTags.HARVEST)
+		.tag(TinkerItemTags.HARVEST_PRIMARY)
+		.tag(TinkerItemTags.STONE_HARVEST)
+		.tag(TinkerItemTags.INTERACTABLE)
+		.tag(TinkerItemTags.INTERACTABLE_RIGHT)
 })
