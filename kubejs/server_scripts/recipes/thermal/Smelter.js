0fa1de576e1f97be782b8f29c94a9e302151b73d
Qi-Month <dy757410@163.com>
2025-08-13 22:03:21 +0800

    recipe code

--- kubejs/server_scripts/recipes/thermal/Smelter.js
+++ kubejs/server_scripts/recipes/thermal/Smelter.js
@@ -1,11 +1,9 @@
 ServerEvents.recipes((event) => {
-	let { thermal, kubejs } = event.recipes
+	let { thermal } = event.recipes
 
-    thermal.smelter(
-        "thermal:enderium_ingot",
-        [
-            "#forge:dusts/diamond",
-        ["#forge:dusts/siltsteel","#forge:ingots/siltsteel"],
-        ["#forge:dusts/ender_pearl","minecraft:ender_pearl"]
-    ])
+	thermal.smelter("thermal:enderium_ingot", [
+		"#forge:dusts/diamond",
+		["#forge:dusts/siltsteel", "#forge:ingots/siltsteel"],
+		["#forge:dusts/ender_pearl", "minecraft:ender_pearl"]
+	])
 })
