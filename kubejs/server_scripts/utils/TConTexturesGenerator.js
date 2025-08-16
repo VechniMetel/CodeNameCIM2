cd243f47fe340b28fd16c112aa6b2be27fbcc989
Qi-Month <dy757410@163.com>
2025-08-16 18:19:40 +0800

    代码优化

--- kubejs/server_scripts/utils/TConTexturesGenerator.js
+++ kubejs/server_scripts/utils/TConTexturesGenerator.js
@@ -1,174 +1,167 @@
 addTConTexture(
-    "andesite_alloy",
-    "FFA9AFA1",
-    "FFE6E6DB",
-    "FFC9CABA",
-    "FF829789",
-    "FF5E6963",
-    "FF4A5451",
-    "FF2B3635",
-    "FF000000"
+	"andesite_alloy",
+	"A9AFA1",
+	"A9AFA1",
+	"E6E6DB",
+	"C9CABA",
+	"829789",
+	"5E6963",
+	"4A5451",
+	"2B3635"
 )
 addTConTexture(
-    "brass",
-    "F9CB68",
-    "FFFCE892",
-    "FFF8CA67",
-    "FFB37748",
-    "FF97593C",
-    "FF75452C",
-    "FFD09F54",
-    "FF5F2E24"
+	"brass",
+	"F9CB68",
+	"F9CB68",
+	"FCE892",
+	"F8CA67",
+	"B37748",
+	"97593C",
+	"75452C",
+	"D09F54"
 )
 addTConTexture(
-    "industrial_iron",
-    "444444",
-    "FF989797",
-    "FF757171",
-    "FF676161",
-    "FF444444",
-    "FF494848",
-    "FF3F3E42",
-    "FF353438"
+	"industrial_iron",
+	"444444",
+	"444444",
+	"989797",
+	"757171",
+	"676161",
+	"444444",
+	"494848",
+	"3F3E42"
 )
 addTConTexture(
-    "cast_iron",
-    "4C4C4C",
-    "FF4C4C4C",
-    "FF3C3C3C",
-    "FF343434",
-    "FF2B2B2B",
-    "FF232323",
-    "FF1A1A1A",
-    "FF131313"
+	"cast_iron",
+	"4C4C4C",
+	"4C4C4C",
+	"4C4C4C",
+	"3C3C3C",
+	"343434",
+	"2B2B2B",
+	"232323",
+	"1A1A1A"
 )
 addTConTexture(
-    "stainless_steel",
-    "708090",
-    "FF708090",
-    "FF75452C",
-    "FF97593C",
-    "FFB37748",
-    "FFD09F54",
-    "FFF8CA67",
-    "FFFCE892"
+	"stainless_steel",
+	"708090",
+	"708090",
+	"708090",
+	"75452C",
+	"97593C",
+	"B37748",
+	"D09F54",
+	"F8CA67"
 )
 addTConTexture(
-    "desh",
-    "9E4539",
-    "FFE9BA5D",
-    "FFD68D4D",
-    "FFC77142",
-    "FF9E4539",
-    "FF7A3045",
-    "FF9E4539",
-    "FF7A3045"
+	"desh",
+	"9E4539",
+	"9E4539",
+	"E9BA5D",
+	"D68D4D",
+	"C77142",
+	"9E4539",
+	"7A3045",
+	"9E4539"
 )
 addTConTexture(
-    "ostrum",
-    "775360",
-    "FFA86C73",
-    "FF925E64",
-    "FF775360",
-    "FF553E4B",
-    "FF42313D",
-    "FF553E4B",
-    "FF42313D"
+	"ostrum",
+	"775360",
+	"775360",
+	"A86C73",
+	"925E64",
+	"775360",
+	"553E4B",
+	"42313D",
+	"553E4B"
 )
 addTConTexture(
-    "calorite",
-    "9E1F3F",
-    "FFDF6D5C",
-    "FFCB4E4F",
-    "FFB83145",
-    "FF9E1F3F",
-    "FF6A1534",
-    "FF6A1534",
-    "FF470D2F"
+	"calorite",
+	"9E1F3F",
+	"9E1F3F",
+	"DF6D5C",
+	"CB4E4F",
+	"B83145",
+	"9E1F3F",
+	"6A1534",
+	"470D2F"
 )
 addTConTexture(
-    "shadow_steel",
-    "38304D",
-    "FF635D71",
-    "FF575366",
-    "FF4D4861",
-    "FF38304D",
-    "FF221C34",
-    "FF140E2A",
-    "FF0B0719"
+	"shadow_steel",
+	"38304D",
+	"38304D",
+	"635D71",
+	"575366",
+	"4D4861",
+	"38304D",
+	"221C34",
+	"140E2A"
 )
 addTConTexture(
-    "refined_radiance",
-    "FFFFF6",
-    "FFFFFFF6",
-    "FFFFFFF6",
-    "FFFFFFF6",
-    "FFF5FAE1",
-    "FFD5DEC8",
-    "FFBBC4A2",
-    "FFA2A98C"
+	"refined_radiance",
+	"FFFFF6",
+	"FFFFF6",
+	"FFFFF6",
+	"FFFFF6",
+	"F5FAE1",
+	"D5DEC8",
+	"BBC4A2",
+	"A2A98C"
 )
 
 function addTConTexture(name, colour, first, second, third, forth, fifth, sixth, seventh) {
-    JsonIO.write(`kubejs/assets/${global.namespace}/tinkering/materials/${name}.json`, {
-        "color": colour,
-        "fallbacks": [
-            "metal"
-        ],
-        "generator": {
-            "supported_stats": [
-                "tconstruct:head",
-                "tconstruct:handle",
-                "tconstruct:binding",
-                "tconstruct:repair_kit",
-                "tconstruct:armor_plating",
-                "tconstruct:plating_helmet",
-                "tconstruct:plating_chestplate",
-                "tconstruct:plating_leggings",
-                "tconstruct:plating_boots",
-                "tconstruct:plating_shield",
-                "tconstruct:maille",
-                "tconstruct:armor_maille",
-                "tconstruct:limb",
-                "tconstruct:grip"
-            ],
-            "transformer": {
-                "type": "tconstruct:recolor_sprite",
-                "color_mapping": {
-                    "type": "tconstruct:grey_to_color",
-                    "palette": [
-                        {
-                            "color": seventh,
-                            "grey": 0
-                        },
-                        {
-                            "color": sixth,
-                            "grey": 63
-                        },
-                        {
-                            "color": fifth,
-                            "grey": 102
-                        },
-                        {
-                            "color": forth,
-                            "grey": 140
-                        },
-                        {
-                            "color": third,
-                            "grey": 178
-                        },
-                        {
-                            "color": second,
-                            "grey": 216
-                        },
-                        {
-                            "color": first,
-                            "grey": 255
-                        }
-                    ]
-                }
-            },
-            "variant": true
-        }
-    })
+	let paletteColors = [
+		first,
+		second,
+		third,
+		forth,
+		fifth,
+		sixth,
+		seventh
+	]
+	paletteColors.map((color) => {
+		return color.startsWith("FF") ? c : `FF${color}`
+	})
+
+	const FILE_PATH = `kubejs/assets/${global.namespace}/tinkering/materials/${name}.json`
+	JsonIO.write(FILE_PATH, {
+		"color": colour.startsWith("FF") ? colour : `FF${colour}`,
+		"fallbacks": [
+			"metal"
+		],
+		"generator": {
+			"supported_stats": [
+				"tconstruct:head",
+				"tconstruct:handle",
+				"tconstruct:binding",
+				"tconstruct:repair_kit",
+				"tconstruct:armor_plating",
+				"tconstruct:plating_helmet",
+				"tconstruct:plating_chestplate",
+				"tconstruct:plating_leggings",
+				"tconstruct:plating_boots",
+				"tconstruct:plating_shield",
+				"tconstruct:maille",
+				"tconstruct:armor_maille",
+				"tconstruct:limb",
+				"tconstruct:grip"
+			],
+			"transformer": {
+				"type": "tconstruct:recolor_sprite",
+				"color_mapping": {
+					"type": "tconstruct:grey_to_color",
+					"palette": [
+						{ "color": paletteColors[6], "grey": 0 },
+						{ "color": paletteColors[5], "grey": 63 },
+						{ "color": paletteColors[4], "grey": 102 },
+						{ "color": paletteColors[3], "grey": 140 },
+						{ "color": paletteColors[2], "grey": 178 },
+						{ "color": paletteColors[1], "grey": 216 },
+						{ "color": paletteColors[0], "grey": 255 }
+					]
+				}
+			},
+			"variant": true
+		}
+	})
 }
