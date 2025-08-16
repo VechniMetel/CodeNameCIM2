55dd1aad6f1ed5afa9bd9c93db78cdba7115c4e1
Qi-Month <dy757410@163.com>
2025-08-16 18:04:41 +0800

    注册优化

--- kubejs/startup_scripts/register/item/Incomplete.js
+++ kubejs/startup_scripts/register/item/Incomplete.js
@@ -1,11 +1,14 @@
 StartupEvents.registry("item", (event) => {
-    // mek未完成电路板
-	event.create(`${global.namespace}:incomplete_advanced_control_circuit`)
+	function addItem(name) {
+		return event.create(`${global.namespace}:${name}`)
+	}
+	// mek未完成电路板
+	addItem("incomplete_advanced_control_circuit")
 		.texture(`${global.namespace}:item/material/advanced_control_circuit`)
 
-	event.create(`${global.namespace}:incomplete_elite_control_circuit`)
+	addItem("incomplete_elite_control_circuit")
 		.texture(`${global.namespace}:item/material/elite_control_circuit`)
 
-	event.create(`${global.namespace}:incomplete_ultimate_control_circuit`)
+	addItem("incomplete_ultimate_control_circuit")
 		.texture(`${global.namespace}:item/material/ultimate_control_circuit`)
 })
