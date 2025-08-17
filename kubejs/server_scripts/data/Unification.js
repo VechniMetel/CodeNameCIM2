4fa57b8ecf5d478e11ef4e31a2b9b724418b1713
Unknown_Entity_ <Unknown_Entity_@outlook.com>
2025-08-17 23:03:13 +0800

    sth

--- kubejs/server_scripts/data/Unification.js
+++ kubejs/server_scripts/data/Unification.js
@@ -1,23 +1,23 @@
-ServerEvents.highPriorityData((event) => {
-	let materialType = [
-		"ingots",
-		"plates",
-		"nuggets",
-		"storage_blocks",
-		"gears",
-		"dusts",
-		"rods"
-	]
-	materialType.forEach((type) => {
-		global.metalGroup.forEach((material) => {
-			let ids = Ingredient.of(`#forge:${type}/${material}`).getItemIds()
-			if (ids.length > 0) {
-				event.addJson(`oei:replacements/${material}_${type}.json`, {
-					"matchItems": [`#forge:${type}/${material}`],
-					"resultItems": ids[0]
-				})
-				console.log("aaa")
-			}
-		})
-	})
-})
+// ServerEvents.highPriorityData((event) => {
+// 	let materialType = [
+// 		"ingots",
+// 		"plates",
+// 		"nuggets",
+// 		"storage_blocks",
+// 		"gears",
+// 		"dusts",
+// 		"rods"
+// 	]
+// 	materialType.forEach((type) => {
+// 		global.metalGroup.forEach((material) => {
+// 			let ids = Ingredient.of(`#forge:${type}/${material}`).getItemIds()
+// 			if (ids.length > 0) {
+// 				event.addJson(`oei:replacements/${material}_${type}.json`, {
+// 					"matchItems": [`#forge:${type}/${material}`],
+// 					"resultItems": ids[0]
+// 				})
+// 				console.log("aaa")
+// 			}
+// 		})
+// 	})
+// })
--- kubejs/server_scripts/recipes/alone/Others.js
+++ kubejs/server_scripts/recipes/alone/Others.js
@@ -193,4 +193,44 @@ ServerEvents.recipes((event) => {
 		A: "cmi:peat",
 		B: "#forge:rods/wooden"
 	}).id("cmi:peat_torch")
+
+	// pipez item/fluid/nergy/gas/all
+	kubejs.shaped("16x pipez:item_pipe",[
+		"ABA"
+	],{
+		A:"#forge:plates/invar",
+		B:"#forge:chests"
+	}).id("pipez:item_pipe")
+
+	kubejs.shaped("16x pipez:fluid_pipe",[
+		"ABA"
+	],{
+		A:"#forge:plates/invar",
+		B:"minecraft:bucket"
+	}
+	).id("pipez:fluid_pipe")
+
+	kubejs.shaped("16x pipez:energy_pipe",[
+		"ABA"
+	],{
+		A:"#forge:plates/invar",
+		B:"minecraft:redstone"
+	}
+	).id("pipez:energy_pipe")
+
+	kubejs.shaped("16x pipez:gas_pipe",[
+		"ABA"
+	],{
+		A:"#forge:plates/invar",
+		B:'mekanism:alloy_infused'
+	}
+	).id("pipez:gas_pipe")
+
+	kubejs.shapeless("4x pipez:universal_pipe",[
+		"pipez:item_pipe",
+		"pipez:fluid_pipe",
+		"pipez:energy_pipe",
+		"pipez:gas_pipe",
+	]
+	).id("pipez:universal_pipe")
 })
