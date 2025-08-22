c6fb6aafc69ae38bcfbac424ee3c4f03dbe98685
Qi-Month <dy757410@163.com>
2025-08-22 21:11:00 +0800

    MacOS Fix?

--- kubejs/startup_scripts/event/GetDateModifyTitle.js
+++ kubejs/startup_scripts/event/GetDateModifyTitle.js
@@ -1,13 +1,15 @@
 // priority: 1
 
 let $LocalDateTime = Java.loadClass("java.time.LocalDateTime")
+let $System = Java.loadClass("java.lang.System")
 
 StartupEvents.postInit((event) => {
 	let month = $LocalDateTime.now().getMonthValue()
 	let day = $LocalDateTime.now().getDayOfMonth()
+	let getSystemOsName = $System.getProperty("os.name").toLowerCase()
 
 	if (Platform.isClientEnvironment()) {
-		if (month === 4 && day === 1) {
+		if (month === 4 && day === 1 && !getSystemOsName.contains("mac")) {
 			Client.window.setTitle("Create: Infinity Mechanism")
 		} else {
 			Client.window.setTitle("Create: Mechanism and Innovation")
