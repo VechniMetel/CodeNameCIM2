5bded1722a5b1e15061ef39dc520c4b9b1001eae
Qi-Month <dy757410@163.com>
2025-08-28 23:53:15 +0800

    判断客户端

--- kubejs/startup_scripts/register/Function.js
+++ kubejs/startup_scripts/register/Function.js
@@ -2,11 +2,13 @@
 
 let FluidBucketItemModel = {
 	generate: function (name) {
-		let file = `kubejs/assets/${global.namespace}/models/item/${name}_bucket.json`
-		JsonIO.write(file, {
-			"parent": "forge:item/bucket_drip",
-			"loader": "forge:fluid_container",
-			"fluid": `${global.namespace}:${name}`
-		})
+		if (Platform.isClientEnvironment()) {
+			let file = `kubejs/assets/${global.namespace}/models/item/${name}_bucket.json`
+			JsonIO.write(file, {
+				"parent": "forge:item/bucket_drip",
+				"loader": "forge:fluid_container",
+				"fluid": `${global.namespace}:${name}`
+			})
+		}
 	}
 }
