6f40bbcb9febb094ff964aac8cc284829eef13f0
VechniMetel <3512546747@qq.com>
2025-09-02 17:18:03 +0800

    修改数值

--- kubejs/server_scripts/data/DieselEngineFuelTypes.js
+++ kubejs/server_scripts/data/DieselEngineFuelTypes.js
@@ -2,27 +2,26 @@ ServerEvents.highPriorityData((event) => {
 
 	// 植物油
 	addDieselEngineFuelType("#forge:plantoil", 1)
-		.normal(64, 16384, 1)
-		.modular(64, 32768, 1)
-		.huge(96, 65536, 1)
+		.normal(32, 16384, 1)
+		.modular(32, 32768, 1)
+		.huge(48, 65536, 1)
 
 	// 酒精
 	addDieselEngineFuelType("#forge:ethanol", 1)
-		.normal(96, 16384, 1)
-		.modular(96, 32768, 1)
-		.huge(128, 65536, 1)
+		.normal(48, 16384, 1)
+		.modular(48, 32768, 1)
+		.huge(64, 65536, 1)
 
 	// 生物柴油
 	addDieselEngineFuelType("#forge:biodiesel", 1)
-		.normal(96, 18432, 1)
-		.modular(96, 36864, 1)
-		.huge(128, 73728, 1)
-
+		.normal(64, 18432, 1)
+		.modular(64, 36864, 1)
+		.huge(96, 73728, 1)
 
 	// 汽油
 	addDieselEngineFuelType("#forge:gasoline", 1)
-		.normal(128, 18432, 1)
-		.modular(128, 36864, 1)
+		.normal(96, 18432, 1)
+		.modular(96, 36864, 1)
 		.huge(128, 73728, 1)
 
 	// 柴油
