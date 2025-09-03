fd2e08df4ab887fd389a3245d8291d31c7846c69
Qi-Month <dy757410@163.com>
2025-09-03 15:28:52 +0800

    含油淤泥

--- kubejs/client_scripts/lang/Chinese.js
+++ kubejs/client_scripts/lang/Chinese.js
@@ -118,6 +118,7 @@ ClientEvents.lang("zh_cn", (event) => {
 	addFluidLanguage("trinitrotoluene_solution", "三硝基甲苯溶液")
 	addFluidLanguage("fluid_nitroglycerine", "液态硝化甘油")
 	addFluidLanguage("crystal_catalyt", "晶体催生剂")
+	addFluidLanguage("oily_sludge", "含油淤泥")
 
 	addMaterialLanguage("andesite_alloy", "安山合金")
 	addMaterialLanguage("cinderslime", "余烬史莱姆")
