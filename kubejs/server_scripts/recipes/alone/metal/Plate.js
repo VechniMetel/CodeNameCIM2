f85f174b940305070b46d0a4bbc2a43677e84b85
随机构件 <oooooooo_zane@163.com>
2025-09-04 21:10:51 +0800

    整理了添加和移除金属配方的代码，并删除了熔炼相关的金属配方
    修改了关于MEK倍矿的代码的细节

---
+++ kubejs/server_scripts/recipes/alone/metal/Plate.js
@@ -0,0 +1,39 @@
+ServerEvents.recipes((event) => {
+    let {
+        create,
+        thermal,
+        immersiveengineering
+    } = event.recipes
+
+    global.metalGroup.forEach((metal) => {
+        const INGOT = `#forge:ingots/${metal}`
+        const PLATE = `#forge:plates/${metal}`
+
+        if (IngredientUtils.isNotNull(PLATE)) {
+			create.pressing(PLATE, [
+				INGOT
+			])
+			thermal.press(PLATE, [
+				INGOT
+			])
+			immersiveengineering.metal_press(PLATE)
+				.input(INGOT)
+				.mold("immersiveengineering:mold_plate")
+		} else {
+			console.warn(`No plate found for ${metal}!`)
+		}
+
+		event.remove([
+			{
+				type: "create:pressing",
+				output: PLATE
+			}, {
+				type: "thermal:press",
+				output: PLATE
+			}, {
+				type: "immersiveengineering:metal_press",
+				output: PLATE
+			}
+		])
+    })
+})
--- kubejs/server_scripts/recipes/mekanism/Processing.js
+++ kubejs/server_scripts/recipes/mekanism/Processing.js
@@ -1,61 +1,64 @@
-let materials = ["copper", "iron", "zinc", "aluminum", "nickel", "platinum", "chromium", "vanadium", "tin", "silver", "gold", "lead", "uranium", "cobalt", "osmium"]
+let materials = [
+    "copper", 
+    "iron", 
+    "zinc", 
+    "aluminum", 
+    "nickel", 
+    "platinum", 
+    "chromium", 
+    "vanadium", 
+    "tin", 
+    "silver", 
+    "gold", 
+    "lead", 
+    "uranium", 
+    "cobalt", 
+    "osmium"
+]
 
 materials.forEach((material) => {
 
-    let clumpIds = Ingredient.of(`#mekanism:clumps/${material}`).getItemIds()
-    let dirtyDustIds = Ingredient.of(`#mekanism:dirty_dusts/${material}`).getItemIds()
-    let dustIds = Ingredient.of(`#forge:dusts/${material}`).getItemIds()
+    const SHARD = `#mekanism:shards/${material}`
+    const CLUMP = `#mekanism:clumps/${material}`
+    const DIRTY_DUST = `#mekanism:dirty_dusts/${material}`
+    const DUST = `#forge:dusts/${material}`
 
     ServerEvents.recipes((event) => {
-        let { mekanism, kubejs } = event.recipes
+        let { mekanism } = event.recipes
 
-        if (IngredientUtils.isNotNull(`#forge:raw_materials/${material}`)) {
-            mekanism.purifying(
-                Item.of(clumpIds[0], 2),
-                `#forge:raw_materials/${material}`,
-                "2x cmi:nitroglycerine"
-            )
-                .id(`mekanism:processing/${material}/clump/from_raw_ore`)
-        }
+        mekanism.purifying(
+            `2x ${CLUMP}`,
+            `#forge:raw_materials/${material}`,
+            "2x cmi:nitroglycerine"
+        ).id(`mekanism:processing/${material}/clump/from_raw_ore`)
 
-        if (IngredientUtils.isNotNull(`#forge:storage_blocks/raw_${material}`)) {
-            mekanism.purifying(
-                Item.of(clumpIds[0], 18),
-                `#forge:storage_blocks/raw_${material}`,
-                "2x cmi:nitroglycerine"
-            )
-                .id(`mekanism:processing/${material}/clump/from_raw_block`)
-        }
+        mekanism.purifying(
+            `18x ${CLUMP}`,
+            `#forge:storage_blocks/raw_${material}`,
+            "2x cmi:nitroglycerine"
+        ).id(`mekanism:processing/${material}/clump/from_raw_block`)
 
-        if (IngredientUtils.isNotNull(`#forge:ores/${material}`)) {
-            mekanism.purifying(
-                Item.of(clumpIds[0], 3),
-                `#forge:ores/${material}`,
-                "2x cmi:nitroglycerine"
-            )
-                .id(`mekanism:processing/${material}/clump/from_ore`)
-        }
+        mekanism.purifying(
+            `3x ${CLUMP}`,
+            `#forge:ores/${material}`,
+            "2x cmi:nitroglycerine"
+        ).id(`mekanism:processing/${material}/clump/from_ore`)
 
-        if (IngredientUtils.isNotNull(`#mekanism:shards/${material}`)) {
-            mekanism.purifying(
-                Item.of(clumpIds[0], 1),
-                `#mekanism:shards/${material}`,
-                "1x cmi:nitroglycerine"
-            )
-                .id(`mekanism:processing/${material}/clump/from_shard`)
-        }
+        mekanism.purifying(
+            CLUMP,
+            SHARD,
+            "cmi:nitroglycerine"
+        ).id(`mekanism:processing/${material}/clump/from_shard`)
 
         mekanism.crushing(
-            dirtyDustIds[0],
-            `#mekanism:clumps/${material}`
-        )
-            .id(`mekanism:processing/${material}/dirty_dust/from_clump`)
+            DIRTY_DUST,
+            CLUMP
+        ).id(`mekanism:processing/${material}/dirty_dust/from_clump`)
 
         mekanism.enriching(
-            dustIds[0],
-            `#mekanism:dirty_dusts/${material}`
-        )
-            .id(`mekanism:processing/${material}/dust/from_dirty_dust`)
+            DUST,
+            DIRTY_DUST
+        ).id(`mekanism:processing/${material}/dust/from_dirty_dust`)
 
     })
 })
