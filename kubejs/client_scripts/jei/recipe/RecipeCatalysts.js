655ec12d30b6db824002d669e222da41f202b7a9
Qi-Month <dy757410@163.com>
2025-09-08 17:38:29 +0800

    jei

--- kubejs/client_scripts/jei/recipe/RecipeCatalysts.js
+++ kubejs/client_scripts/jei/recipe/RecipeCatalysts.js
@@ -1,10 +1,17 @@
 JEIAddedEvents.registerRecipeCatalysts((event) => {
 	let { data } = event
 
-	let acceleratorTypeId = ResourceLocation.parse("cmi:accelerator")
-	let recipeType = data.jeiHelpers.getRecipeType(acceleratorTypeId).get()
+	function addJeiRecipeIndex(id, item, displayName) {
+		let type = data.jeiHelpers.getRecipeType(ResourceLocation.parse(id)).get()
+		let key = Component.translatable(`recipe.jei.recipe.cmi.${displayName}`)
 
-	let key = Component.translatable("recipe.jei.recipe.cmi.accelerator")
-	data["addRecipeCatalyst(net.minecraft.world.item.ItemStack,mezz.jei.api.recipe.RecipeType[])"]
-		(Item.of("cmi:accelerator").setHoverName(key), recipeType)
+		if (displayName === undefined) {
+			return data["addRecipeCatalyst(net.minecraft.world.item.ItemStack,mezz.jei.api.recipe.RecipeType[])"]
+				(Item.of(item), type)
+		}
+		return data["addRecipeCatalyst(net.minecraft.world.item.ItemStack,mezz.jei.api.recipe.RecipeType[])"]
+			(Item.of(item).setHoverName(key), type)
+	}
+
+	addJeiRecipeIndex("cmi:accelerator", "cmi:accelerator", "accelerator")
 })
