26a4a6432a7dc6ecf8dee5e1c2c8783a9363c30b
随机构件 <oooooooo_zane@163.com>
2025-09-12 21:07:32 +0800

    修复了热力粉碎机屏障磨粉的问题

--- kubejs/server_scripts/recipes/modpack/Processing.js
+++ kubejs/server_scripts/recipes/modpack/Processing.js
@@ -1,16 +1,18 @@
 global.metalGroup.forEach((metal) => {
 
-
+    // 锇制作流程中的步骤
     let dustIds = Ingredient.of(`#forge:dusts/${metal}`).getItemIds()
     let ingotIds = Ingredient.of(`#forge:ingots/${metal}`).getItemIds()
 
     ServerEvents.recipes((event) => {
         let { thermal } = event.recipes
         if (IngredientUtils.isNotNull(`#forge:dusts/${metal}`)) {
-            thermal.pulverizer(
-                dustIds[0],
-                `#mekanism:clumps/${metal}`
-            )
+            if(IngredientUtils.isNotNull(`#mekanism:clumps/${metal}`)) {
+                thermal.pulverizer(
+                    dustIds[0],
+                    `#mekanism:clumps/${metal}`
+                )
+            }
             thermal.crystallizer(
                 ingotIds[0],
                 [
