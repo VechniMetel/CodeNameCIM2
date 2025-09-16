7372a0421de900c81fb427228d470f3f808eec62
VechniMetel <3512546747@qq.com>
2025-09-16 16:06:38 +0800

    没能成功添加支柱

--- kubejs/server_scripts/event/IndustrialPlatform.js
+++ kubejs/server_scripts/event/IndustrialPlatform.js
@@ -9,14 +9,14 @@ BlockEvents.rightClicked("cmi:industrial_platform", (event) => {
     const FINZ = Math.floor(POSZ / 16) * 16
 
     if (item.is("minecraft:cobblestone")) {
-        level.runCommandSilent(`/fill ${FINX} ${POSY - 1} ${FINZ} ${FINX + 15} ${POSY + 11} ${FINZ + 15} air`)
+        level.runCommandSilent(`/fill ${FINX} ${POSY} ${FINZ} ${FINX + 15} ${POSY + 11} ${FINZ + 15} air`)
         level.runCommandSilent(`/place template cmi:industrial_platform/light ${FINX} ${POSY - 1} ${FINZ}`)
-        event.cancel()
         player.swing()
+        event.cancel()
     }
 
     else if (item.is("minecraft:cobbled_deepslate")) {
-        level.runCommandSilent(`/fill ${FINX} ${POSY - 1} ${FINZ} ${FINX + 31} ${POSY + 11} ${FINZ + 31} air`)
+        level.runCommandSilent(`/fill ${FINX} ${POSY} ${FINZ} ${FINX + 31} ${POSY + 11} ${FINZ + 31} air`)
         level.runCommandSilent(`/place template cmi:industrial_platform/heavy ${FINX} ${POSY - 1} ${FINZ}`)
         event.cancel()
         player.swing()
