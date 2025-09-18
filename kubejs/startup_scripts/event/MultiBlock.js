5868d6631156d6e3d30ec1d8c868ce9c95985ef7
Qi-Month <dy757410@163.com>
2025-09-18 15:07:19 +0800

    修改函数名

--- kubejs/startup_scripts/event/MultiBlock.js
+++ kubejs/startup_scripts/event/MultiBlock.js
@@ -1,23 +1,25 @@
-// This script can be hot reloaded with "kjs reload starup_scripts" or "-re"
-// 这个脚本可以用"kjs reload starup_scripts"或"-re"进行热重载
+/*
+This script can be hot reloaded with "kjs reload starup_scripts" or "-re"
+这个脚本可以用"kjs reload starup_scripts"或"-re"进行热重载
+*/
 IEEvents.multiblockForm((event) => {
 	let { player, multiblock } = event
 
 	const IE = "immersiveengineering"
 	const II = "immersiveindustry"
 
-	function multiblockId(namespace, id) {
-		// let alloySmelter = "immersiveengineering:multiblocks/alloy_smelter"
+	function getMultiblockId(namespace, id) {
 		return ResourceLocation.fromNamespaceAndPath(namespace, `multiblocks/${id}`)
 	}
 	function disableMultiblock(multiblockId) {
 		if (multiblock === multiblockId) {
 			let tran = Component.translatable("message.cmi.disabled_multiblock").bold()
 			player.displayClientMessage(tran, true)
+
 			event.cancel()
 			return
 		}
 	}
 
-	disableMultiblock(multiblockId(IE, "alloy_smelter"))
+	disableMultiblock(getMultiblockId(IE, "alloy_smelter"))
 })
