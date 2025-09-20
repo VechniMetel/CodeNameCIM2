702948d6d1da750ca1fa9c83a8cbf545a6f75f7b
Qi-Month <dy757410@163.com>
2025-09-20 23:39:14 +0800

    删除冗余核构件脚本

--- kubejs/server_scripts/event/mechanism/Nuclear.js
+++
@@ -1,8 +0,0 @@
-ItemEvents.rightClicked("cmi:nuclear_mechanism", (event) => {
-	// 只在服务端运行
-	if (event.level.isClientSide()) {
-		return
-	}
-	event.player.mergeNbt({ ForgeCaps: { "mekanism:radiation": { radiation: 0 } } })
-	event.player.swing()
-})
