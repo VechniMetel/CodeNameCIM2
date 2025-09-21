d268ba55f71f5e62183f9d795529e317759ac748
随机构件 <oooooooo_zane@163.com>
2025-09-21 22:51:29 +0800

    随机随机构件！

--- kubejs/startup_scripts/register/item/mechanism/Common.js
+++ kubejs/startup_scripts/register/item/mechanism/Common.js
@@ -39,17 +39,6 @@ StartupEvents.registry("item", (event) => {
 		"potion"
 	]
 	
-	//仅注册完成状态的构件 没有驱动
-	let completeMechanismRegister = [
-		"basic_random",
-		"mechanical_random",
-		"engineering_random",
-		"flux_random",
-		"magical_random",
-		"quantum_random",
-		"mekanism_random",
-		"space_random"
-	]
 	mechanismRegister.forEach((material) => {
 		event.create(`${global.namespace}:${material}_mechanism`)
 			.texture(`${global.namespace}:item/mechanism/complete/${material}`)
@@ -58,11 +47,5 @@ StartupEvents.registry("item", (event) => {
 		event.create(`${global.namespace}:incomplete_${material}_mechanism`, "create:sequenced_assembly")
 			.texture(`${global.namespace}:item/mechanism/incomplete/${material}`)
 			.tag("create:incomplete_mechanisms")
-	})
-	completeMechanismRegister.forEach((material) => {
-		event.create(`${global.namespace}:${material}_mechanism`)
-			.texture(`${global.namespace}:item/mechanism/complete/${material}`)
-			.tag("create:mechanisms")
-			.displayName(Component.translatable(`${global.namespace}:${material}_mechanism`))
 	})
 })
---
+++ kubejs/startup_scripts/register/item/mechanism/Random.js
@@ -0,0 +1,20 @@
+StartupEvents.registry("item", (event) => {
+    // 批量注册随机构件
+	let randomMechanismRegister = [
+		"basic",
+		"mechanical",
+		"engineering",
+		"flux",
+		"magical",
+		"quantum",
+		"mekanism",
+		"space",
+        "random"
+	]
+    randomMechanismRegister.forEach((material) => {
+		event.create(`${global.namespace}:${material}_random_mechanism`)
+			.texture(`${global.namespace}:item/mechanism/random/${material}`)
+			.tag("cmi:random_mechanisms")
+			.displayName(Component.translatable(`${global.namespace}:${material}_random_mechanism`))
+	})
+})
