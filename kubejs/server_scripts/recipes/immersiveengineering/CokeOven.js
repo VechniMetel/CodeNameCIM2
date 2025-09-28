c301b25411affe0fed00226d2cdfc8f661942e6b
Qi-Month <dy757410@163.com>
2025-09-28 12:19:28 +0800

    焦炉

--- kubejs/server_scripts/recipes/immersiveengineering/CokeOven.js
+++ kubejs/server_scripts/recipes/immersiveengineering/CokeOven.js
@@ -1,9 +1,28 @@
 ServerEvents.recipes((event) => {
 	let { immersiveengineering } = event.recipes
 
+	// immersiveengineering.coke_oven("#forge:coal_coke")
+	// 	.time(900 * 0.75)
+	// 	.input("minecraft:charcoal")
+	// 	.creosote(500)
+
+	immersiveengineering.coke_oven("minecraft:charcoal")
+		.time(900 * 0.75)
+		.input("#minecraft:logs")
+		.creosote(250)
+		.id("immersiveengineering:cokeoven/charcoal")
+
 	immersiveengineering.coke_oven("#forge:coal_coke")
-		.input("minecraft:charcoal")
+		.time(500 * 0.75)
+		.input(["minecraft:coal", "minecraft:charcoal"])
 		.creosote(500)
+		.id("immersiveengineering:cokeoven/coke")
+
+	immersiveengineering.coke_oven("#forge:storage_blocks/coal_coke")
+		.time(16200 * 0.75)
+		.input(["#forge:storage_blocks/coal", "#forge:storage_blocks/charcoal"])
+		.creosote(5000)
+		.id("immersiveengineering:cokeoven/coke_block")
 
 	event.custom({
 		"type": "immersiveindustry:rotary_kiln",
