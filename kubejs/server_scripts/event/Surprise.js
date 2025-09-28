d55e3223fd79f06a318cc4944e02f7c9765c699f
Qi-Month <dy757410@163.com>
2025-09-28 11:26:11 +0800

    Surprise.js

--- kubejs/server_scripts/event/Surprise.js
+++ kubejs/server_scripts/event/Surprise.js
@@ -1,6 +1,6 @@
 let $LocalDateTime = Java.loadClass("java.time.LocalDateTime")
 
-PlayerEvents.loggedIn(event => {
+PlayerEvents.loggedIn((event) => {
 
 	let { player, server } = event
 	let month = $LocalDateTime.now().getMonthValue()
@@ -12,10 +12,10 @@ PlayerEvents.loggedIn(event => {
 				.clickOpenUrl("https://www.bilibili.com/video/BV1UT42167xb"))
 		})
 	}
+
 	if (month === 10 && day === 1) {
 		server.scheduleInTicks(5, () => {
 			player.tell(Component.translatable(`message.${global.namespace}.chinese_national_day`))
 		})
 	}
-
 })
