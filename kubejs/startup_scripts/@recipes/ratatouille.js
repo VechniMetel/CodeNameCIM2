f68e7a41d7499731ba4a9a86d1595147e0df8988
Qi-Month <dy757410@163.com>
2025-09-29 21:37:05 +0800

    尝试

--- kubejs/startup_scripts/@recipes/ratatouille.js
+++ kubejs/startup_scripts/@recipes/ratatouille.js
@@ -14,3 +14,33 @@ new Schema("ratatouille:freezing")
 	.simpleKey("results", "outputItemArray")
 	.simpleKey("ingredients", "inputItemArray")
 	.simpleKey("processingTime", "intNumber", 100)
+
+let $RecipeSchema =
+	Java.loadClass("dev.latvian.mods.kubejs.recipe.schema.RecipeSchema")
+
+StartupEvents.recipeSchemaRegistry((event) => {
+	let { components } = event
+
+	/**
+	 * @template {keyof Special.RecipeComponents} T
+	 * @param {T} type
+	 * @param {string} key
+	 * @param {boolean} [optional]
+	 */
+	function buildRecipeSchema(type, key, optional) {
+		let builder = components.get(type)().key(key)
+		if (optional !== undefined) {
+			builder = builder.defaultOptional()
+		}
+		return new $RecipeSchema(builder)
+	}
+
+	let RecipeTypeSchema = {
+		a: {
+			b: function () {
+				buildRecipeSchema("", "", true)
+			}
+		}
+	}
+	// event.register("", RecipeTypeSchema.a.b())
+})
