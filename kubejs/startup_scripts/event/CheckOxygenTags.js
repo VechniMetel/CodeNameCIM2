ff8eaa37e228764c4e284a0c48b5f406598e5b8a
Qi-Month <dy757410@163.com>
2025-10-01 20:52:06 +0800

    CheckOxygenTags.js

---
+++ kubejs/startup_scripts/event/CheckOxygenTags.js
@@ -0,0 +1,18 @@
+// let $AdAstraEvents$EntityOxygenEvent =
+// 	Java.loadClass("earth.terrarium.adastra.api.events.AdAstraEvents$EntityOxygenEvent")
+
+// StartupEvents.init((event) => {
+// 	$AdAstraEvents$EntityOxygenEvent.register((entity, hasOxygen) => {
+// 		let getArmorItem = {
+// 			head: entity.getHeadArmorItem(),
+// 			chest: entity.getChestArmorItem(),
+// 			legs: entity.getLegsArmorItem(),
+// 			feet: entity.getFeetArmorItem()
+// 		}
+
+// 		if (!hasOxygen) {
+// 			return getArmorItem.head ===
+// 		}
+// 		return true
+// 	})
+// })
