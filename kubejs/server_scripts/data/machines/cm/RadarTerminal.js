f8029376c8bcde3e7d53af17b851c8d194182552
Qi-Month <dy757410@163.com>
2025-10-09 01:25:13 +0800

    GUI

--- kubejs/server_scripts/data/machines/cm/RadarTerminal.js
+++ kubejs/server_scripts/data/machines/cm/RadarTerminal.js
@@ -2,9 +2,6 @@ ServerEvents.highPriorityData((event) => {
 	event.addJson(`${global.namespace}:machines/radar_terminal.json`, {
 		"name": "block.cmi.radar_terminal",
 		"appearance": {
-			"idle": {
-				"block": "cmi:machine/radar_terminal/off"
-			},
 			"running": {
 				"block": "cmi:machine/radar_terminal/on"
 			},
@@ -14,45 +11,59 @@ ServerEvents.highPriorityData((event) => {
 			"paused": {
 				"block": "cmi:machine/radar_terminal/off"
 			},
-			"interaction_sound": "minecraft:iron_block",
-			"requires_tool": true,
-			"mining_level": "minecraft:needs_diamond_tool"
+			"hardness": -1,
+			"interaction_sound": {},
+			"mining_level": "minecraft:needs_diamond_tool",
+			"tool_type": "minecraft:mineable/pickaxe",
+			"resistance": 114514,
+			"idle": {
+				"block": "cmi:machine/radar_terminal/off"
+			}
 		},
 		"components": [
 			{
 				"type": "custommachinery:energy",
-				"id": "energy_input",
-				"mode": "input",
 				"capacity": 100000,
 				"config": {
-					"default": "both"
+					"TOP": "BOTH",
+					"BOTTOM": "BOTH",
+					"FRONT": "BOTH",
+					"RIGHT": "BOTH",
+					"BACK": "BOTH",
+					"LEFT": "BOTH"
 				}
 			},
 			{
 				"type": "custommachinery:item",
-				"id": "cell_input",
-				"mode": "input",
 				"filter": [
-					"#cmi:cells"
-				]
+					"cmi:empty_cell"
+				],
+				"whitelist": true,
+				"id": "cell_input",
+				"mode": "input"
+			},
+			{
+				"type": "custommachinery:item",
+				"id": "output",
+				"mode": "output"
+			},
+			{
+				"type": "custommachinery:item",
+				"variant": "custommachinery:energy",
+				"id": "battery",
+				"mode": "input"
 			}
 		],
 		"gui": [
 			{
-				"type": "custommachinery:texture",
-				"texture": "custommachinery:textures/gui/base_background.png",
-				"x": 0,
-				"y": 0
+				"type": "custommachinery:background",
+				"width": 256,
+				"height": 166
 			},
 			{
 				"type": "custommachinery:player_inventory",
 				"x": 47,
 				"y": 80
-			},
-			{
-				"type": "custommachinery:progress",
-				"x": 110,
-				"y": 45
 			},
 			{
 				"type": "custommachinery:status",
@@ -61,22 +72,82 @@ ServerEvents.highPriorityData((event) => {
 			},
 			{
 				"type": "custommachinery:slot",
-				"texture": "cmi:textures/gui/cell_slot.png",
-				"id": "cell_input",
-				"x": 69,
-				"y": 34
+				"id": "output",
+				"x": 150,
+				"y": 44
+			},
+			{
+				"type": "custommachinery:progress",
+				"x": 120,
+				"y": 45
 			},
 			{
 				"type": "custommachinery:slot",
-				"id": "output",
-				"x": 140,
+				"id": "cell_input",
+				"texture": "cmi:textures/gui/cell_slot.png",
+				"x": 99,
 				"y": 44
 			},
 			{
 				"type": "custommachinery:energy",
 				"id": "energy_input",
-				"x": 165,
-				"y": 30
+				"x": 78,
+				"y": 31
+			},
+			{
+				"type": "custommachinery:slot",
+				"id": "battery",
+				"texture": "cmi:textures/gui/battery_slot.png",
+				"x": 52,
+				"y": 44
+			}
+		],
+		"jei": [
+			{
+				"type": "custommachinery:background",
+				"width": 256,
+				"height": 166
+			},
+			{
+				"type": "custommachinery:player_inventory",
+				"x": 47,
+				"y": 80
+			},
+			{
+				"type": "custommachinery:status",
+				"x": 236,
+				"y": 5
+			},
+			{
+				"type": "custommachinery:slot",
+				"id": "output",
+				"x": 150,
+				"y": 44
+			},
+			{
+				"type": "custommachinery:progress",
+				"x": 120,
+				"y": 45
+			},
+			{
+				"type": "custommachinery:slot",
+				"id": "cell_input",
+				"texture": "cmi:textures/gui/cell_slot.png",
+				"x": 99,
+				"y": 44
+			},
+			{
+				"type": "custommachinery:energy",
+				"id": "energy_input",
+				"x": 78,
+				"y": 31
+			},
+			{
+				"type": "custommachinery:slot",
+				"id": "battery",
+				"texture": "cmi:textures/gui/battery_slot.png",
+				"x": 52,
+				"y": 44
 			}
 		]
 	})
