8ea8c990637cdaed481e8a566506991d049db683
Qi-Month <dy757410@163.com>
2025-10-11 18:42:08 +0800

    沟槽的KubeJS, 注册BlockEntity没有走ForgeCapability那一套, 想要实现物流还得手搓

--- kubejs/startup_scripts/register/block/MachineBlock.js
+++ kubejs/startup_scripts/register/block/MachineBlock.js
@@ -56,9 +56,40 @@ StartupEvents.registry("block", (event) => {
 			.defaultCutout()
 			.blockEntity((entity) => {
 				let mechanism = Ingredient.of(`#create:mechanisms/${type}`)
+
 				entity.enableSync()
 				entity.inventory(9, 3, mechanism)
 				entity.rightClickOpensInventory()
+				entity.attachCapability(CapabilityBuilder.ITEM.blockEntity()
+					// 允许漏斗或管道提取物品
+					.extractItem((entity, slot, amount, simulate) => {
+						let { inventory } = entity
+
+						return inventory.extractItem(slot, amount, simulate)
+					})
+					// 允许漏斗或管道插入物品
+					.insertItem((entity, slot, stack, simulate) => {
+						let { inventory } = entity
+
+						return inventory.insertItem(slot, stack, simulate)
+					})
+					// 获取指定槽位的物品
+					.getStackInSlot((entity, slot) => {
+						return entity.inventory.getStackInSlot(slot)
+					})
+					// 槽位堆叠上限
+					.getSlotLimit((entity, slot) => {
+						return entity.inventory.getSlotLimit(slot)
+					})
+					// 槽位总数
+					.getSlots((entity) => {
+						return entity.inventory.slots
+					})
+					// 判断物品是否能放入槽位
+					.isItemValid((entity, slot, stack) => {
+						return entity.inventory.isItemValid(slot, stack)
+					})
+				)
 			})
 	})
 })
