6536949f25abaa7a3383344e205287682f23e151
VechniMetel <3512546747@qq.com>
2025-10-25 23:17:27 +0800

    dev成员模型重置

--- kubejs/startup_scripts/register/block/Developer.js
+++ kubejs/startup_scripts/register/block/Developer.js
@@ -1,51 +1,84 @@
 StartupEvents.registry("block", (event) => {
-	function addDeveloperDoll(name) {
-		let registerDeveloperDoll =
-			event.create(`${global.namespace}:${name}`, "cardinal")
-				.soundType(SoundType.WOOL)
-				.hardness(0)
-				.resistance(1)
-				.model(`${global.namespace}:block/dev/${name}`)
-				.waterlogged()
-				.notSolid()
-				.defaultCutout()
-				.notSolid()
-				.noCollision()
-				.tag(`${global.namespace}:dev_doll`)
 
-		return registerDeveloperDoll
+	function developerDollmodel(dollName, skinType) {
+
+		const PARENT = `cmi:block/dev/${skinType}`
+		const SKIN = `cmi:block/dev/${dollName}`
+
+		return {
+			parent: PARENT,
+			textures: {
+				"skin": SKIN
+			}
+		}
 	}
+
+	function addDeveloperDoll(name, type) {
+
+		let blockBuilder = event.create(`${global.namespace}:${name}`, "cardinal")
+
+		blockBuilder.soundType(SoundType.WOOL)
+		blockBuilder.hardness(0)
+		blockBuilder.resistance(1)
+
+		blockBuilder.waterlogged()
+		blockBuilder.notSolid()
+		blockBuilder.defaultCutout()
+		blockBuilder.notSolid()
+		blockBuilder.noCollision()
+
+		blockBuilder.tag(`${global.namespace}:dev_doll`)
+		blockBuilder.modelJson = developerDollmodel(name, type)
+		blockBuilder.displayName(Component.translatable(`${global.namespace}:${name}`))
+
+	}
+
 	// Developer
+
 	// UE
-	addDeveloperDoll("unknown_entity_")
+	addDeveloperDoll("unknown_entity_", "slim")
+
 	// 柒月
-	addDeveloperDoll("qi_month")
+	addDeveloperDoll("qi_month", "slim")
+
 	// 中微子
-	addDeveloperDoll("vechni_metel")
+	addDeveloperDoll("vechni_metel", "slim")
+
 	// 1174
-	addDeveloperDoll("117458866249")
+	addDeveloperDoll("117458866249", "wide")
+
 	// FiberOptics
-	addDeveloperDoll("fiber_optics")
+	addDeveloperDoll("fiber_optics", "slim")
+
 	// Random Mechanism
-	addDeveloperDoll("random_mechanism")
+	addDeveloperDoll("random_mechanism", "slim")
+
 	// MF
-	addDeveloperDoll("sakura_star_cn")
+	addDeveloperDoll("sakura_star_cn", "slim")
+
 	// Dropper
-	addDeveloperDoll("dropper_qwq")
+	addDeveloperDoll("dropper_qwq", "wide")
+
 	// 燕栖
-	addDeveloperDoll("ye_anqing")
+	addDeveloperDoll("ye_anqing", "slim")
+
 	// 亓才孑
-	addDeveloperDoll("qicaijie")
+	addDeveloperDoll("qicaijie", "slim")
 
 	// music
+
 	// DontKillBelalus
-	addDeveloperDoll("dont_kill_belalus")
+	addDeveloperDoll("dont_kill_belalus", "wide")
 
 	// Other
+
 	// Aleegos
-	addDeveloperDoll("aleegos")
+	addDeveloperDoll("aleegos", "slim")
+
 	// shi_xiao_bai_na
-	addDeveloperDoll("shi_xiao_bai_na")
+	addDeveloperDoll("shi_xiao_bai_na", "slim")
+
 	// hanhan
-	addDeveloperDoll("hanhan_ovo")
+	addDeveloperDoll("hanhan_ovo", "wide")
+
 })
