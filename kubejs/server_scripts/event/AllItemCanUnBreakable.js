14c46a2d71d01731010d115b2bfdbad79f92b9ec
Qi-Month <dy757410@163.com>
2025-10-27 21:13:01 +0800

    变量名调整

--- kubejs/server_scripts/event/AllItemCanUnBreakable.js
+++ kubejs/server_scripts/event/AllItemCanUnBreakable.js
@@ -1,11 +1,13 @@
 PlayerEvents.loggedIn((event) => {
 	let { player } = event
 	const UNBREAKABLE_TAG = "cmi:unbreakable"
+	const UNBREAKABLE_NBT_NAME = "Unbreakable"
 
 	for (let i = 0; i < player.inventory.getContainerSize(); i++) {
 		let item = player.inventory.getStackInSlot(i)
+
 		if (item.hasTag(UNBREAKABLE_TAG)) {
-			if (!item.nbt || !item.nbt.getBoolean("Unbreakable")) {
+			if (!item.nbt || !item.nbt.getBoolean(UNBREAKABLE_NBT_NAME)) {
 				let nbt = item.nbt || {}
 				nbt.Unbreakable = true
 				item.nbt = nbt
