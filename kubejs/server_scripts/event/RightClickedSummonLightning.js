9c90a929534076fd8c37b204111f22b7c6a9e23a
Qi-Month <dy757410@163.com>
2025-11-02 22:05:44 +0800

    增加可读性

--- kubejs/server_scripts/event/RightClickedSummonLightning.js
+++ kubejs/server_scripts/event/RightClickedSummonLightning.js
@@ -4,7 +4,10 @@ BlockEvents.rightClicked((event) => {
 	let { block, player, level } = event
 
 	if (!level.isClientSide()) {
-		if (player.mainHandItem.id === "cmi:coil_mechanism" && block.id === "cmi:accelerator") {
+		if (player.mainHandItem.id ===
+			"cmi:coil_mechanism" &&
+			block.id === "cmi:accelerator"
+		) {
 			const LIGHTNING = $EntityType.LIGHTNING_BOLT.create(level)
 
 			LIGHTNING.setPos(block.x + 0.5, block.y, block.z + 0.5)
