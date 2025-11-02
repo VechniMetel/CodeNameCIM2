9f1706058287f9e27b3526371514ffe8bb26a90e
Unknown_Entity_ <Unknown_Entity_@outlook.com>
2025-11-02 22:29:59 +0800

    ftb

--- kubejs/server_scripts/recipes/machine/SimpleCentrifuge.js
+++ kubejs/server_scripts/recipes/machine/SimpleCentrifuge.js
@@ -79,43 +79,4 @@ ServerEvents.recipes((event) => {
         ]
     })
 
-    event.custom({
-        "type": "custommachinery:custom_machine",
-        "machine": `${global.namespace}:simple_centrifuge`,
-        "time": 200,
-        "hidden": false,
-        "requirements": [
-            {
-                "type": "custommachinery:item",
-                "mode": "input",
-                "item": "thermal:sapphire_dust",
-                "amount": 1
-            },
-            {
-                "type": "custommachinery:item",
-                "mode": "input",
-                "item": "thermal:ruby_dust",
-                "amount": 1
-            },
-            {
-                "type": "custommachinery:fluid",
-                "mode": "input",
-                "fluid": "minecraft:lava",
-                "amount": 500
-            },
-            {
-                "type": "custommachinery:item",
-                "mode": "output",
-                "item": "immersiveengineering:dust_aluminum",
-                "amount": 2
-            },
-            {
-                "type": "custommachinery:contraption",
-                "mode": "input",
-                "speed": 64,
-                "stress": 4,
-                "scaling": true
-            }
-        ]
-    })
 })
