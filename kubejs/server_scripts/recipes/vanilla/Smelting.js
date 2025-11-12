c03cfa119e580a8d995ce5b0156cdcf1a2d4b446
VechniMetel <3512546747@qq.com>
2025-11-12 18:40:49 +0800

    删除多余配方

--- kubejs/server_scripts/recipes/vanilla/Smelting.js
+++
@@ -1,8 +0,0 @@
-ServerEvents.recipes((event) => {
-	SmeltingRecipe.all(event, "cmi:combustion_medium_block", [
-		[
-			"#forge:storage_blocks/coal",
-			"#forge:storage_blocks/charcoal"
-		]
-	])
-})
