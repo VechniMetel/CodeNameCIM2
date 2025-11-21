67a47f3ed212ab388a0ffb1a5d6f920009c389cd
Qi-Month <dy757410@163.com>
2025-11-21 23:08:34 +0800

    气体

--- kubejs/server_scripts/recipes/machine/ChemicalGasExtractor.js
+++ kubejs/server_scripts/recipes/machine/ChemicalGasExtractor.js
@@ -16,7 +16,7 @@ ServerEvents.recipes((event) => {
 				"filter": "minecraft:the_nether"
 			}
 		]
-	})
+	}).id("cmi:chemical_gas_extractor/sulfide_gas_mixture")
 
 	// 氡气
 	event.custom({
@@ -50,5 +50,39 @@ ServerEvents.recipes((event) => {
 				"filter": "cmi:dionysus"
 			}
 		]
-	})
+	}).id("cmi:chemical_gas_extractor/radon")
+
+	// 汞
+	event.custom({
+		"type": "custommachinery:custom_machine",
+		"machine": `${global.namespace}:chemical_gas_extractor`,
+		"time": 1,
+		"hidden": false,
+		"requirements": [
+			{
+				"type": "custommachinery:gas",
+				"mode": "output",
+				"chemical": "cmi:mercury",
+				"amount": 2
+			},
+			{
+				"type": "custommachinery:structure",
+				"pattern": [
+					[
+						"A"
+					],
+					[
+						"m"
+					]
+				],
+				"keys": {
+					"A": "cmi:mercury_geothermal_vent[smoke_type=3]"
+				}
+			},
+			{
+				"type": "custommachinery:dimension",
+				"filter": "cmi:mercury"
+			}
+		]
+	}).id("cmi:chemical_gas_extractor/mercury")
 })
