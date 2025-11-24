fae8b99f4354a865024e34b2fc6d0b5078bd4e98
Qi-Month <dy757410@163.com>
2025-11-24 20:26:59 +0800

    jsdoc

--- kubejs/server_scripts/recipes/tconstruct/Fuel.js
+++ kubejs/server_scripts/recipes/tconstruct/Fuel.js
@@ -25,12 +25,12 @@ ServerEvents.recipes((event) => {
 	 * 例如烈焰血的就是"tconstruct:smeltery/melting/fuel/blaze"
 	 * 当然你要是硬要用.id()去替换我也阻止不了你
 	 * 
-	 * @param {Internal.InputFluid_} fluid 流体id
+	 * @param {Internal.FluidStackJS} fluid 流体id
 	 * @param {Number} temperature 温度
 	 * @returns {MeltingFuelTConstruct} 匠魂燃料配方
 	 */
 	function addFuel(fluid, temperature) {
-		return tconstruct.melting_fuel(fluid.withAmount(50))
+		return tconstruct.melting_fuel(fluid.withChance(50))
 			.duration(150)
 			.rate(temperature / 100)
 			.temperature(temperature)
