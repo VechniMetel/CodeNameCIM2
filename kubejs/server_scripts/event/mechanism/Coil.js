cc3d325c34346bbf5e81f24c3427df15e760e775
Qi-Month <dy757410@163.com>
2025-12-15 23:06:41 +0800

    LIGHTNING

--- kubejs/server_scripts/event/mechanism/Coil.js
+++ kubejs/server_scripts/event/mechanism/Coil.js
@@ -4,15 +4,10 @@ let $EntityType =
 BlockEvents.rightClicked((event) => {
 	let { block, player, level } = event
 
-	if (!level.isClientSide()) {
-		if (
-			player.mainHandItem.id === "cmi:coil_mechanism" &&
-			block.id === "cmi:accelerator"
-		) {
-			const LIGHTNING = $EntityType.LIGHTNING_BOLT.create(level)
+	if (player.mainHandItem.id === "cmi:coil_mechanism" && block.id === "cmi:accelerator") {
+		const LIGHTNING = $EntityType.LIGHTNING_BOLT.create(level)
 
-			LIGHTNING.setPos(block.x + 0.5, block.y, block.z + 0.5)
-			level.addFreshEntity(LIGHTNING)
-		}
+		LIGHTNING.setPos(block.x + 0.5, block.y, block.z + 0.5)
+		level.addFreshEntity(LIGHTNING)
 	}
 })
