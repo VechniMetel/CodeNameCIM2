f3624bcb562d0a620cf8eed66f92d67f7a2ae861
Qi-Month <dy757410@163.com>
2025-12-17 01:34:03 +0800

    func

--- kubejs/client_scripts/tooltip/FluidTooltip.js
+++ kubejs/client_scripts/tooltip/FluidTooltip.js
@@ -1,5 +1,14 @@
 NebulaEvents.fluidTooltip((event) => {
-	event.add(Fluid.of("cmi:sea_water"), (tooltip) => {
+	addFluidTooltip("cmi:sea_water", (tooltip) => {
 		tooltip.add(Component.translatable("aaa"))
 	})
+
+	/**
+	 * 
+	 * @param {Internal.FluidStackJS_} fluid 
+	 * @param {Internal.Consumer_<Internal.List<net.minecraft.network.chat.Component>>} hanlder 
+	 */
+	function addFluidTooltip(fluid, hanlder) {
+		event.add(Fluid.of(fluid), hanlder)
+	}
 })
