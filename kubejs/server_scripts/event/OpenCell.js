1c7b3db028f0da2bc0b7c1317a671c0f88c77859
Qi-Month <dy757410@163.com>
2025-12-23 01:24:00 +0800

    Cell

--- kubejs/server_scripts/event/OpenCell.js
+++ kubejs/server_scripts/event/OpenCell.js
@@ -1,37 +1,53 @@
+let cellMap = [
+	{
+		item: "cmi:tier_1_aviation_complete_cell",
+		uiId: "cmi:cell_1",
+		texture: "cmi:textures/gui/cell/tier_1.png"
+	},
+	{
+		item: "cmi:tier_2_aviation_complete_cell",
+		uiId: "cmi:cell_2",
+		texture: "cmi:textures/gui/cell/tier_2.png"
+	},
+	{
+		item: "cmi:tier_3_aviation_complete_cell",
+		uiId: "cmi:cell_3",
+		texture: "cmi:textures/gui/cell/tier_3.png"
+	},
+	{
+		item: "cmi:tier_4_aviation_complete_cell",
+		uiId: "cmi:cell_4",
+		texture: "cmi:textures/gui/cell/tier_4.png"
+	}
+]
+
+// 右键打开 UI
 ItemEvents.rightClicked((event) => {
 	let { item, player } = event
 
-	item.getEnchantmentLevel()
-
-	if (item === "cmi:tier_1_aviation_complete_cell") {
+	let cell = cellMap.find((c) => {
+		c.item === item
+	})
+	if (cell) {
 		player.swing()
-		ItemUIFactory.INSTANCE.openUI(player, event.hand, "cmi:cell_1")
+		ItemUIFactory.INSTANCE.openUI(player, event.hand, cell.uiId)
 	}
 })
 
-let CellUI = {
-	cell_1: function () {
-		let group = new WidgetGroup()
-
-		const UI_WIDTH = (416 / 2) + (416 / 8)
-		const UI_HEIGHT = (512 / 2) + (512 / 8)
-
-		let mainTexturePath = "cmi:textures/gui/cell/tier_1.png"
-		let mainTexture = new ResourceTexture(
-			mainTexturePath,
-			0,
-			0,
-			1,
-			1
-		)
-
-		group.setSize(UI_WIDTH, UI_HEIGHT)
-		group.setBackground(mainTexture)
-
-		return group
-	}
+// UI 工厂函数
+function createCellUI(texturePath) {
+	let group = new WidgetGroup()
+	const UI_WIDTH = (416 / 2) + (416 / 8)
+	const UI_HEIGHT = (512 / 2) + (512 / 8)
+	let mainTexture = new ResourceTexture(texturePath, 0, 0, 1, 1)
+	group.setSize(UI_WIDTH, UI_HEIGHT)
+	group.setBackground(mainTexture)
+	return group
 }
 
-LDLibUI.item("cmi:cell_1", (event) => {
-	event.success(CellUI.cell_1())
+// 注册 UI
+cellMap.forEach((cell) => {
+	LDLibUI.item(cell.uiId, (event) => {
+		event.success(createCellUI(cell.texture))
+	})
 })
